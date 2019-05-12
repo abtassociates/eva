164389dbc9daf1ed34faf86df166057f5cf601de
Genelle Denzin <genelle@eyedarts.net>
2019-05-12 18:06:38 -0400

    updated global file so it works for elevated

--- global.R
+++ global.R
@@ -4,17 +4,23 @@ library(shinyWidgets)
 library(lubridate)
 library(shinydashboard)
 library(scales)
+library(zoo)
 
-updatedate <- file.info("data/Client.csv")$mtime
+updatedate <- file.info("data/Utilization.RData")$mtime
+
+filebeginningdate <- 
+  file.info("data/Utilization.RData")$mtime - years(2)
 
 load("data/Utilization.RData")
 
 load("data/QPR_SPDATs.RData")
 
-ProviderAverages <- SPDATsByProject %>%
-  filter(served_between(SPDATsByProject, ReportStart, ReportEnd)) %>%
-  select(EnrollmentID, ProjectName, ScoreAdjusted) %>%
-  group_by(ProjectName) %>%
-  summarise(AverageScore = round(mean(ScoreAdjusted), 1),
-            EnrollmentCount = n())
+choices_month <-
+  format(seq.Date(
+    from = as.Date(floor_date(today(), unit = "month") - years(2)),
+    by = "month",
+    length.out = 24
+  ), "%b %Y")
+
+
 
