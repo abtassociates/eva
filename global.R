27271064e62292efcf2e2d277100431eb4b46cca
Genelle Denzin <genelle@eyedarts.net>
2019-06-02 23:29:17 -0400

    simplified the file deleted the choices_month object since the utilization tab is not using it anymore

--- global.R
+++ global.R
@@ -8,19 +8,12 @@ library(zoo)
 
 updatedate <- file.info("data/Utilization.RData")$mtime
 
-filebeginningdate <- 
-  file.info("data/Utilization.RData")$mtime - years(2)
+filebeginningdate <- updatedate - years(2)
 
 load("data/Utilization.RData")
 
 load("data/QPR_SPDATs.RData")
 
-choices_month <-
-  format(seq.Date(
-    from = as.Date(floor_date(today(), unit = "month") - years(2)),
-    by = "month",
-    length.out = 24
-  ), "%b %Y")
 
 
 
