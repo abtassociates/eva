d9d192fb1752c717faab3e5d0ce92627e64cc0c6
Genelle Denzin <genelle@eyedarts.net>
2019-06-03 12:20:15 -0400

    changed the way updatedate is handled, so it's returning the date/time the actual data was refreshed, not the date/time the .rdata file was last compiled

--- global.R
+++ global.R
@@ -6,14 +6,11 @@ library(shinydashboard)
 library(scales)
 library(zoo)
 
-updatedate <- file.info("data/Utilization.RData")$mtime
-
-filebeginningdate <- updatedate - years(2)
-
 load("data/Utilization.RData")
 
 load("data/QPR_SPDATs.RData")
 
+filebeginningdate <- updatedate - years(2)
 
 
 
