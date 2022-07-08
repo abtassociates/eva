8df63caf520c2776d12dc2648db6aa4fae92c0cf
Genelle Denzin <Genelle_Denzin@abtassoc.com>
2022-07-08 13:46:29 -0400

    simplified age_years function

--- 00_functions.R
+++ 00_functions.R
@@ -15,46 +15,10 @@
 
 # Age Function ------------------------------------------------------------
 
-age_years <- function(earlier, later)
+age_years <- function(dob, compare_date)
 {
-  lt <- data.frame(earlier, later)
-  age <- as.numeric(format(lt[,2],format="%Y")) - as.numeric(format(lt[,1],format="%Y"))
+  floor(decimal_date(compare_date) - decimal_date(dob))
   
-  dayOnLaterYear <- ifelse(
-    format(lt[, 1], format = "%m-%d") != "02-29",
-    as.Date(paste(
-      format(lt[, 2], format = "%Y"), "-",
-      format(lt[, 1], format = "%m-%d"),
-      sep = ""
-    )),
-    ifelse(
-      as.numeric(format(later, format = "%Y")) %%
-        400 == 0 |
-        as.numeric(format(later,
-                          format =
-                            "%Y")) %%
-        100 != 0 &
-        as.numeric(format(later, format = "%Y")) %%
-        4 == 0,
-      as.Date(paste(
-        format(lt[, 2], format = "%Y"),
-        "-",
-        format(lt[, 1], format =
-                 "%m-%d"),
-        sep = ""
-      )),
-      as.Date(paste(
-        format(lt[, 2], format = "%Y"),
-        "-",
-        "02-28",
-        sep = ""
-      ))
-    )
-  )
-  
-  age[which(dayOnLaterYear > lt$later)] <- age[which(dayOnLaterYear > lt$later)] - 1
-  
-  age
 }
 
 
