362cc3071ca39f7eb9b82551120364d994b371b8
Alex Silverman <silvermana@aneprr-wxo.ad.abt.local>
2022-10-03 17:06:47 -0400

    don't need any of the name and SSN stuff (since it will be hashed) and Personal id should already be character based on the import col_types. If not, that will be flagged in the Data Integrity checker.

--- 00_initial_data_prep.R
+++ 00_initial_data_prep.R
@@ -4,48 +4,48 @@ Project <- Project %>%
             by = "OrganizationID")
 
 # Client ------------------------------------------------------------------
-Client <- Client %>%
-  mutate(
-    FirstName = case_when(
-      NameDataQuality %in% c(8, 9) ~ "DKR",
-      NameDataQuality == 2 ~ "Partial",
-      NameDataQuality == 99 |
-        is.na(NameDataQuality) |
-        FirstName == "Anonymous" ~ "Missing",
-      !(
-        NameDataQuality %in% c(2, 8, 9, 99) |
-          is.na(NameDataQuality) |
-          FirstName == "Anonymous"
-      ) ~ "ok"
-    ),
-    LastName = NULL,
-    MiddleName = NULL,
-    NameSuffix = NULL,
-    SSN = case_when(
-      (is.na(SSN) & !SSNDataQuality %in% c(8, 9)) |
-        is.na(SSNDataQuality) | SSNDataQuality == 99 ~ "Missing",
-      SSNDataQuality %in% c(8, 9) ~ "DKR",
-      (nchar(SSN) != 9 & SSNDataQuality != 2) |
-        substr(SSN, 1, 3) %in% c("000", "666") |
-        substr(SSN, 1, 1) == 9 |
-        substr(SSN, 4, 5) == "00" |
-        substr(SSN, 6, 9) == "0000" |
-        SSNDataQuality == 2 |
-        SSN %in% c(
-          111111111,
-          222222222,
-          333333333,
-          444444444,
-          555555555,
-          777777777,
-          888888888,
-          123456789
-        ) ~ "Invalid",
-      SSNDataQuality == 2 & nchar(SSN) != 9 ~ "Incomplete"
-    ),
-    PersonalID = as.character(PersonalID)
-  ) %>%
-  mutate(SSN = case_when(is.na(SSN) ~ "ok", !is.na(SSN) ~ SSN))
+# Client <- Client %>%
+  # mutate(
+    # FirstName = case_when(
+    #   NameDataQuality %in% c(8, 9) ~ "DKR",
+    #   NameDataQuality == 2 ~ "Partial",
+    #   NameDataQuality == 99 |
+    #     is.na(NameDataQuality) |
+    #     FirstName == "Anonymous" ~ "Missing",
+    #   !(
+    #     NameDataQuality %in% c(2, 8, 9, 99) |
+    #       is.na(NameDataQuality) |
+    #       FirstName == "Anonymous"
+    #   ) ~ "ok"
+    # ),
+    # LastName = NULL,
+    # MiddleName = NULL,
+    # NameSuffix = NULL,
+    # SSN = case_when(
+    #   (is.na(SSN) & !SSNDataQuality %in% c(8, 9)) |
+    #     is.na(SSNDataQuality) | SSNDataQuality == 99 ~ "Missing",
+    #   SSNDataQuality %in% c(8, 9) ~ "DKR",
+    #   (nchar(SSN) != 9 & SSNDataQuality != 2) |
+    #     substr(SSN, 1, 3) %in% c("000", "666") |
+    #     substr(SSN, 1, 1) == 9 |
+    #     substr(SSN, 4, 5) == "00" |
+    #     substr(SSN, 6, 9) == "0000" |
+    #     SSNDataQuality == 2 |
+    #     SSN %in% c(
+    #       111111111,
+    #       222222222,
+    #       333333333,
+    #       444444444,
+    #       555555555,
+    #       777777777,
+    #       888888888,
+    #       123456789
+    #     ) ~ "Invalid",
+    #   SSNDataQuality == 2 & nchar(SSN) != 9 ~ "Incomplete"
+    # ),
+    # PersonalID = as.character(PersonalID)
+  # ) %>%
+  # mutate(SSN = case_when(is.na(SSN) ~ "ok", !is.na(SSN) ~ SSN))
 
 # Enrollment --------------------------------------------------------------
 # Enrollment <- Enrollment %>%
