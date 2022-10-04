dcf5f94f57d2d555f21f270398a74a0bc1e5dc8e
Victoria Lopez <lopezvi@aneprr-wxo.ad.abt.local>
2022-10-04 14:10:39 -0400

    Added in project type 9 (PH-Housing Only) for project_type function

--- 00_functions.R
+++ 00_functions.R
@@ -59,6 +59,7 @@ project_type <- function(ReferenceNo){
     ReferenceNo == 4 ~ "Street Outreach",
     ReferenceNo == 6 ~ "Services Only",
     ReferenceNo == 8 ~ "Safe Haven",
+    ReferenceNo == 9 ~ "PH - Housing Only",
     ReferenceNo == 12 ~ "Prevention",
     ReferenceNo == 13 ~ "Rapid Rehousing",
     ReferenceNo == 14 ~ "Coordinated Entry"
