b1e15ab9ce22c57ec5af730b3d74b9d37b52ba63
Genelle Denzin <Genelle_Denzin@abtassoc.com>
2022-10-24 15:38:43 -0400

    rename app, also align all the tab names to the same naming convention

--- ui.R
+++ ui.R
@@ -16,23 +16,23 @@
 
 dashboardPage(
   skin = "black",
-  dashboardHeader(title = "Stella HMIS"),
+  dashboardHeader(title = "StellaR"),
   dashboardSidebar(
     sidebarMenu(
       id = "sidebarmenuid",
       menuItem("Home",
-               tabName = "homeTab"),
+               tabName = "tabHome"),
       menuItem("Upload Hashed CSV",
-               tabName = "uploadCSV"),
+               tabName = "tabUploadCSV"),
       menuItem("Check PDDEs",
                tabName = "tabPDDE"),
       menuItem("Client Counts",
-                  tabName = "currentProviderLevel"),
+                  tabName = "tabClientCount"),
       menuItem("Data Quality",
                menuSubItem("System-level",
-                           tabName = "dqSystem"),
+                           tabName = "tabDQSystem"),
                menuSubItem("Organization-level",
-                           tabName = "dqTab")
+                           tabName = "tabDQOrg")
         # menuSubItem("Organization-level", 
         #             tabName = "dqOrganization"),
         # menuSubItem("Data Entry Timeliness", 
@@ -58,13 +58,13 @@ dashboardPage(
     ),
     tabItems(
     tabItem(
-      tabName = "homeTab",
+      tabName = "tabHome",
       box(
-        title = "Welcome to Stella HMIS!",
+        title = "Welcome to StellaR!",
         width = 12,
         HTML(
-          "<div>Stella HMIS (Stella H) is intended for local use by HMIS Administrators in Continuums of Care (CoCs) around the U.S. and its territories. 
-          Stella H is designed to help you assess the accuracy and completeness of the data within your HMIS. 
+          "<div>StellaR is intended for local use by HMIS Administrators in Continuums of Care (CoCs) around the U.S. and its territories. 
+          StellaR is designed to help you assess the accuracy and completeness of the data within your HMIS. 
           In future iterations it will also assist communities in analyzing your HMIS performance data, 
           including coordinated entry, if your community utilizes HMIS for this purpose. Use of this tool is not required by HUD.</div>
           <br/>
@@ -80,7 +80,7 @@ dashboardPage(
       width = 12
     ),
     tabItem(
-      tabName = "uploadCSV",
+      tabName = "tabUploadCSV",
       box(
         title = "Edit CoC-specific Settings",
         width = 12,
@@ -182,7 +182,7 @@ dashboardPage(
     )
     ), 
     tabItem(
-      tabName = "currentProviderLevel",
+      tabName = "tabClientCount",
       fluidRow(box(htmlOutput("headerCurrent"), width = 12)),
       fluidRow(box(
         pickerInput(
@@ -288,7 +288,7 @@ dashboardPage(
       ))
     ),
     tabItem(
-      tabName = "dqTab",
+      tabName = "tabDQOrg",
       fluidRow(box(htmlOutput("headerDataQuality"), width = 12)),
       fluidRow(box(
         pickerInput(
@@ -382,7 +382,7 @@ dashboardPage(
       )
     ),
     tabItem(
-      tabName = "dqSystem",
+      tabName = "tabDQSystem",
       fluidRow(box(htmlOutput("headerSystemDQ"), width = 12)),
       fluidRow(
         box(
