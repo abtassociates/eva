6f67772920e43b25abdf712c43afb946c48b5759
Alex Silverman <silvermana@aneprr-xip.ad.abt.local>
2022-10-27 15:56:47 -0400

    Use Project, which already had OrganizationName in it, instead of this initially calculated dataframe. However, this also requires joining with Organization explicitly in one place to pull its VictimServiceProvider field.

--- 00_PDDE_Checker.R
+++ 00_PDDE_Checker.R
@@ -1,21 +1,8 @@
 PDDEcols = c("ProjectID", "ProjectName", "Issue", "Guidance", "OrganizationName")
 
-projectWithOrg <- Project %>% 
-  left_join(Organization, by = "OrganizationID") %>%
-  select(ProjectID, 
-         ProjectName, 
-         ProjectType, 
-         HMISParticipatingProject,
-         OrganizationID, 
-         OrganizationName, 
-         OperatingStartDate, 
-         OperatingEndDate, 
-         VictimServiceProvider
-  )
-
 # Subpop beds = TotalBeds
 subpopNotTotal <- Inventory %>%
-  left_join(projectWithOrg, by = "ProjectID") %>%
+  left_join(Project, by = "ProjectID") %>%
   filter(ProjectType %in% c(1,2,3,8,9,10,13) & 
            (CHVetBedInventory + 
               YouthVetBedInventory + 
@@ -33,7 +20,7 @@ subpopNotTotal <- Inventory %>%
 
 # Missing Operating End Date If a project has no open enrollments and the most recent Enrollment was 30+ days ago
 operatingEndMissing <- Enrollment %>%
-  left_join(projectWithOrg, by = "ProjectID") %>%
+  left_join(Project %>% select(ProjectID, OrganizationName), by = "ProjectID") %>%
   group_by(ProjectID) %>%
   mutate(NumOpenEnrollments = sum(is_null(ExitDate)),
          MostRecentEnrollment = max(ExitDate)
@@ -47,7 +34,7 @@ operatingEndMissing <- Enrollment %>%
 
 # Missing CoC Information Missing address field(s), Missing Geocode, Missing Geography Type, Invalid Zip Code if possible
 missingCoCInfo <- ProjectCoC %>%
-  left_join(projectWithOrg, by = "ProjectID") %>%
+  left_join(Project, by = "ProjectID") %>%
   filter(is_null(Address1) | 
            is_null(City) | 
            is_null(State) | 
@@ -62,7 +49,7 @@ missingCoCInfo <- ProjectCoC %>%
 
 # Missing Inventory Record Is a residential project but has no active inventory for the duration of operating period OR for the reporting period
 missingInventoryRecord <- Inventory %>%
-  left_join(projectWithOrg, by = "ProjectID") %>%
+  left_join(Project, by = "ProjectID") %>%
   filter(ProjectType %in% c(1,2,3,8,9,10,13) & 
            (InventoryStartDate > OperatingEndDate | InventoryEndDate < OperatingStartDate) & 
            (InventoryStartDate > meta_HUDCSV_Export_End | InventoryEndDate < meta_HUDCSV_Export_Start)
@@ -74,7 +61,7 @@ missingInventoryRecord <- Inventory %>%
 
 # Funder.StartDate <= Project.OperatingStartDate
 funderStartOnOrBeforeOperatingStart <- Funder %>%
-  left_join(projectWithOrg, by = "ProjectID") %>%
+  left_join(Project, by = "ProjectID") %>%
   filter(StartDate <= OperatingStartDate) %>%
   mutate(Issue = "Funding period before operating start",
          Guidance = "Funding should begin after the Project begins"
@@ -84,7 +71,7 @@ funderStartOnOrBeforeOperatingStart <- Funder %>%
 # Inventory Start < Operating Start AND
 # Inventory End > Operating End or Null
 inventoryOutsideOperating <- Inventory %>%
-  left_join(projectWithOrg, by = "ProjectID") %>%
+  left_join(Project, by = "ProjectID") %>%
   filter(InventoryStartDate < OperatingStartDate |
            InventoryEndDate > OperatingEndDate) %>%
   mutate(Issue = "Inventory outside operating dates",
@@ -95,6 +82,7 @@ inventoryOutsideOperating <- Inventory %>%
 # HMIS Participating != 1, OR VSP != 0 but client level data in file
 hmisNotParticipatingButClient <- Project %>%
   left_join(unique(Enrollment[c("PersonalID", "ProjectID")]), by = "ProjectID") %>%
+  left_join(Organization %>% select(OrganizationID, VictimServiceProvider), by = "OrganizationID") %>%
   filter((HMISParticipatingProject != 1 | VictimServiceProvider != 0) & !is_null(PersonalID)) %>%
   mutate(Issue = "Is HMIS but has client-level data",
          Guidance = "HMIS Projects should not have client-level data"
@@ -106,7 +94,7 @@ hmisNotParticipatingButClient <- Project %>%
 # Incompatible Funding Source and Project Type Funding Source X can only be used with Project Type Y. Project Type A can only be used with Funding Source B. (this will need a lot more detail, hold on this one)
 # Utilization Hold - Push to next version
 
-
+### Put it together ----
 pdde_main <- rbind(
   subpopNotTotal,
   operatingEndMissing,
