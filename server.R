7be6c4414fda88b42e3361ca515fdff0315bed61
Victoria Lopez <lopezvi@aneprr-wxo.ad.abt.local>
2022-11-16 10:09:23 -0500

    added min back to DateRangeInput so users can't change the date to something before the export's report start date.

--- server.R
+++ server.R
@@ -189,6 +189,7 @@ function(input, output, session) {
                       value = meta_HUDCSV_Export_Start)
       
       updateDateRangeInput(session = session, inputId = "dateRangeCount",
+                           min = meta_HUDCSV_Export_Start,
                            start = meta_HUDCSV_Export_Start,
                            end = meta_HUDCSV_Export_End)
     }
