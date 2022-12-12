c4bee5770d6bb69c442d53f5be532343fd6c42b9
Alex Silverman <silvermana@aneprr-xip.ad.abt.local>
2022-12-12 10:52:33 -0500

    remove the favicon in the upload hashed csv box

--- ui.R
+++ ui.R
@@ -114,9 +114,6 @@ dashboardPage(
           ),  
           box(
             title = "Upload Hashed CSV zip file",
-            HTML('<i class="fa fa-info-circle" 
-                title = "Use the Browse function to direct the app to the file folder containing your zipped CSV.">
-                 </i>'),
             fileInput("imported",
                       label = NULL,
                       multiple = FALSE,
