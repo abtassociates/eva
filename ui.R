dbcf7b300168539721a669dbc069a4734753ec30
alex-silverman <alexsilverman96@gmail.com>
2023-01-23 14:55:10 -0500

    Update ui.R
    fixed the timeout to be 3 minutes again, rather than 5 seconds

--- ui.R
+++ ui.R
@@ -39,7 +39,7 @@ dashboardPage(
       tags$link(rel = "stylesheet", type = "text/css", href = "custom.css"),
       tags$html(lang="en"), #Added as WAVE fix but not considered ideal
       tags$script(HTML("function idleTimer() {
-          var timeoutTime = 5000;
+          var timeoutTime = 180000;
           var t = setTimeout(logout, timeoutTime);
           window.onmousemove = resetTimer; // catches mouse movements
           window.onmousedown = resetTimer; // catches mouse movements
