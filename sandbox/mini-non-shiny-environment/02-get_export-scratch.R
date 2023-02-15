e856f065e7cc20687029ef0187185f1e8ce21155
Genelle Denzin <Genelle_Denzin@abtassoc.com>
2023-02-15 17:18:34 -0500

    changed vector name for clarity

--- sandbox/mini-non-shiny-environment/02-get_export-scratch.R
+++ sandbox/mini-non-shiny-environment/02-get_export-scratch.R
@@ -1,75 +1,75 @@
-directory <- "sandbox/mini-non-shiny-environment/data/"
+data_directory <- "sandbox/mini-non-shiny-environment/data/"
 
 # Read in each file -------------------------------------------------------
 
 Assessment <- 
-  read_csv(paste0(directory, "Assessment.csv"),
+  read_csv(paste0(data_directory, "Assessment.csv"),
            col_types = "cccDcnnnTTcTc")
 
 Client <- 
-  read_csv(paste0(directory, "Client.csv"), 
+  read_csv(paste0(data_directory, "Client.csv"), 
              col_types = "cccccncnDnnnnnnnnnnnnnnnnnnnnnnnnnnnTTcTc")
 
 client_problems <- problems(Client)
 
 CurrentLivingSituation <- 
-  read_csv(paste0(directory, "CurrentLivingSituation.csv"),
+  read_csv(paste0(data_directory, "CurrentLivingSituation.csv"),
              col_types = "cccDncnnnnncTTcTc")
 
 Enrollment <- 
-  read_csv(paste0(directory, "Enrollment.csv"),
+  read_csv(paste0(data_directory, "Enrollment.csv"),
     col_types = 
       "cccDcnnnnnDnnnDDDnnnnccccnnnDnnnncnnnnnnnnnnnncnnnnnnnnnnnnnnnnnnnnTTcTc")
 
 EnrollmentCoC <- 
-  read_csv(paste0(directory, "EnrollmentCoC.csv"),
+  read_csv(paste0(data_directory, "EnrollmentCoC.csv"),
              col_types = "cccccDcnTTcTc")
 
 EmploymentEducation <- 
-  read_csv(paste0(directory, "EmploymentEducation.csv"),
+  read_csv(paste0(data_directory, "EmploymentEducation.csv"),
              col_types = "cccDnnnnnnTTcTc")
 
 Exit <- 
-  read_csv(paste0(directory, "Exit.csv"), col_types = "cccDncnnnnnnnnnnnnnnnnnnnnnnnnnDnnnnnnTTcTc")
+  read_csv(paste0(data_directory, "Exit.csv"), col_types = "cccDncnnnnnnnnnnnnnnnnnnnnnnnnnDnnnnnnTTcTc")
 
 Event <- 
-  read_csv(paste0(directory, "Event.csv"), col_types = "cccDnnncnDTTcTc")
+  read_csv(paste0(data_directory, "Event.csv"), col_types = "cccDnnncnDTTcTc")
 
 Export <- 
-  read_csv(paste0(directory, "Export.csv"), col_types = "cncccccccTDDcccnnn")
+  read_csv(paste0(data_directory, "Export.csv"), col_types = "cncccccccTDDcccnnn")
 
 Funder <- 
-  read_csv(paste0(directory, "Funder.csv"), col_types = "ccnccDDTTcTc")
+  read_csv(paste0(data_directory, "Funder.csv"), col_types = "ccnccDDTTcTc")
 
 HealthAndDV <- 
-  read_csv(paste0(directory, "HealthAndDV.csv"), col_types = "cccDnnnnnnnDnnnnnTTcTc")
+  read_csv(paste0(data_directory, "HealthAndDV.csv"), col_types = "cccDnnnnnnnDnnnnnTTcTc")
 
 IncomeBenefits <- 
-  read_csv(paste0(directory, 
+  read_csv(paste0(data_directory, 
     "IncomeBenefits.csv"),
     col_types =
       "cccDnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnncnnnnnnncnnnnnnnnnnnnnnnnnnnncnnnnnnnnTTcTc")
 
 Inventory <- 
-  read_csv(paste0(directory, "Inventory.csv"), col_types = "cccnnnnnnnnnnnnDDTTcTc")
+  read_csv(paste0(data_directory, "Inventory.csv"), col_types = "cccnnnnnnnnnnnnDDTTcTc")
 
 Organization <- 
-  read_csv(paste0(directory, "Organization.csv"), col_types = "ccncTTcTc")
+  read_csv(paste0(data_directory, "Organization.csv"), col_types = "ccncTTcTc")
 
 Project <- 
-  read_csv(paste0(directory, "Project.csv"), col_types = "ccccDDnnnnnnnnnTTcTc")
+  read_csv(paste0(data_directory, "Project.csv"), col_types = "ccccDDnnnnnnnnnTTcTc")
 
 ProjectCoC <- 
-  read_csv(paste0(directory, "ProjectCoC.csv"), col_types = "cccccccccnTTcTc")
+  read_csv(paste0(data_directory, "ProjectCoC.csv"), col_types = "cccccccccnTTcTc")
 
 Services <- 
-  read_csv(paste0(directory, "Services.csv"), col_types = "cccDnnccnnnTTcTc")
+  read_csv(paste0(data_directory, "Services.csv"), col_types = "cccDnnccnnnTTcTc")
 
 User <- 
-  read_csv(paste0(directory, "User.csv"), col_types = "ccccccTTTc")
+  read_csv(paste0(data_directory, "User.csv"), col_types = "ccccccTTTc")
 
 YouthEducationStatus <- 
-  read_csv(paste0(directory, "YouthEducationStatus.csv"), col_types = "cccDnnnnTTcTc")
+  read_csv(paste0(data_directory, "YouthEducationStatus.csv"), col_types = "cccDnnnnTTcTc")
 
 
 problems <- rbind(
