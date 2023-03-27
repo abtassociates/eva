6aa2e74efdacf385d15d63561ee406016db53f1c
Alex Silverman <silvermana@aneprr-xip.ad.abt.local>
2023-03-27 17:51:07 -0400

    Fixed the code that updates the sandbox environment. Basically this just copies the numbered scripts from the main environment, and then, for those needing extra changes, using a replace_code function it does some specific find+replaces, as well as a general remove of logToConsole.

--- sandbox/mini-non-shiny-environment/updating_sandbox/updating_environment.R
+++ sandbox/mini-non-shiny-environment/updating_sandbox/updating_environment.R
@@ -1,19 +1,13 @@
 library(fs)
 
 # Get the app directory
-app_dir <- getwd()
+app_dir <- "~/DQ_CE/"
 
 # Set the path to the sandbox directory
-sandbox_dir <- file.path(app_dir, "sandbox/mini-non-shiny-environment")
+sandbox_dir <- paste0(app_dir, "sandbox/mini-non-shiny-environment/")
 
-simple_file_copy <- function(fname) {
-  app_version = file.path(app_dir, fname)
-  sandbox_location = file.path(sandbox_dir, fname)
-  file.copy(app_version, sandbox_location)
-  return(TRUE)
-}
-
-replace_code <- function(filepath, replacelist) {
+replace_code <- function(fname, replacelist) {
+  filepath = paste0(sandbox_dir, fname)
   # read in the contents of the script
   lines <- readLines(filepath)
 
@@ -22,21 +16,30 @@ replace_code <- function(filepath, replacelist) {
     lines <- gsub(names(replacelist)[i], replacelist[[i]], lines)
   }
   
+  # Remove lines containing the pattern logToConsole("SOME TEXT HERE")
+  pattern <- "logToConsole\\(\".*\"\\)"
+  remove_lines <- grepl(pattern, lines)
+  lines <- lines[!remove_lines]
+  
   # write the updated script back out to the original file
-  cat(lines, file = filepath, sep = "\n")
-  return(TRUE)
+  writeLines(lines, filepath, sep = "\n")
+  # write the updated script back out to the original file
+  # cat(lines, file = filepath, sep = "\n")
 }
 
-simple_file_copy("hardcodes.R")
+files_to_copy = list(
+  "hardcodes.R",
+  "helper_functions.R",
+  "01_get_Export.R",
+  "02_export_dates.R",
+  "03_file_structure_analysis.R",
+  "04_initial_data_prep.R"
+)
+file.copy(from = paste0(app_dir, files_to_copy),
+          to = paste0(sandbox_dir, files_to_copy),
+          overwrite=TRUE)
 
-simple_file_copy("helper_functions.R")
-helper_script_path <- file.path(sandbox_dir, "helper_functions.R")
-replace_code(helper_script_path,  list("importFile" = "importFileSandbox"))
-
-simple_file_copy("02_export_dates.R")
-simple_file_copy("03_file_structure_analysis.R")
-simple_file_copy("04_initial_data_prep.R")
-simple_file_copy("05_DataQuality.R")
-simple_file_copy("05_DataQuality_functions.R")
-simple_file_copy("06_PDDE_Checker.R")
-simple_file_copy("client_counts_functions.R")
+replace_code("01_get_Export.R",  list("importFile" = "importFileSandbox"))
+replace_code("02_export_dates.R",  list())
+replace_code("03_file_structure_analysis.R",  list())
+replace_code("04_initial_data_prep.R",  list())
