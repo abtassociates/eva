0c247cc2340febf76b5d09edd18f3dc97d0495b0
Alex Silverman <silvermana@aneprr-wxo.ad.abt.local>
2023-04-07 11:52:20 -0400

    mispelled failure() in yaml, and added comments to test-good.R explaining many of my findings

--- tests/shinytest/test-good.R
+++ tests/shinytest/test-good.R
@@ -1,4 +1,14 @@
 customDownload <- function(downloadHandler, fname) {
+  # this allows us to trigger the download code, which thus exports the test values
+  # as specified in server.R (exportTestValues())
+  # but then we delete the xlsx file so they don't get compared
+  # xlsx files seem to retain metadata that changes with each run and thus
+  # the two files are not identical in GitHub Actions, even though identical() says they are
+  # Also, RStudio can't view binary file comparisons interactively the way it can
+  # with other sorts of files
+  # I had tried saving as RDS, but that was having the same comparibility issue 
+  # with GitHubActions.
+  
   app$snapshotDownload(downloadHandler, paste0(fname,".xlsx"))
   #downloadedFile <- import_xlsx(paste0("test-good-current/",fname,".xlsx"))
   #saveRDS(downloadedFile, paste0("test-good-current/",fname,".rds"))
@@ -19,7 +29,11 @@ app$uploadFile(imported = "../test_uploads/HMIS CSV Export - Current Good.zip") 
 app$findElement("button[data-dismiss='modal']")$click()
 Sys.sleep(2)
 customDownload("downloadFileStructureAnalysis","File-Structure-Analysis-Download")
-app$snapshot()
+# without specifying anything here, it will download numerically 
+# incrementing .json and .png files. We will only compare JSON, not png, since
+# png the plots seem to load slightly differently each time which can throw off
+# exact comparisons
+app$snapshot() 
 
 
 app$setInputs(sidebarmenuid = "tabClientCount")
@@ -35,6 +49,7 @@ app$snapshot()
 app$setInputs(sidebarmenuid = "tabDQSystem")
 customDownload("downloadSystemDQReport", "System-DQ-Download")
 
+# waitForValue doesn't seem to work here
 app$setInputs(sidebarmenuid = "tabDQOrg", timeout_=10000)
 customDownload("downloadOrgDQReport", "Org-DQ-Download")
 
