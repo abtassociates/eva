84305c491a898f367dbe7acfe88e958b501a666c
Genelle Denzin <Genelle_Denzin@abtassoc.com>
2023-04-25 14:44:20 -0400

    keeping everything to the metadata-analysis folder that has to do with the metadata analysis, pointing all paths to the correct directories

--- sandbox/scratch-issue-154.R
+++ sandbox/scratch-issue-154.R
@@ -3,10 +3,10 @@ library(lubridate)
 library(janitor)
 
 shiny_log <- 
-  read_log("www/metadata/01-25-2023_pm/eva-shiny-20230125-003304-43997.log")
+  read_log("metadata-analysis/metadata/01-25-2023_pm/eva-shiny-20230125-003304-43997.log")
 
 metadata <- 
-  read_csv("www/metadata/01-25-2023_pm/metadata.csv")
+  read_csv("metadata-analysis/metadata/01-25-2023_pm/metadata.csv")
 
 session_data <- 
-  read_csv("www/metadata/01-25-2023_pm/sessiondata.csv")
+  read_csv("metadata-analysis/metadata/01-25-2023_pm/sessiondata.csv")
