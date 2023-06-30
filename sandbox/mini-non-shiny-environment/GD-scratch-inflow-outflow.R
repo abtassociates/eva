01a118f364ad5bdaff2ff19482775423b56f42d6
Genelle Denzin <Genelle_Denzin@abtassoc.com>
2023-06-30 11:42:18 -0400

    adding in Housed variable, clarifying about if we're wanting to exclude stayers. Currently doesn't work because we need an FY2024 sample data set to work with. (working on that)

--- sandbox/mini-non-shiny-environment/GD-scratch-inflow-outflow.R
+++ sandbox/mini-non-shiny-environment/GD-scratch-inflow-outflow.R
@@ -1,6 +1,5 @@
 
 
-
 fy22_to_fy24_living_situation <- function(value){
   case_when(
     value %in% c(3, 19, 20, 28, 31, 33, 34, 38, 39) ~ 435,
@@ -10,7 +9,7 @@ fy22_to_fy24_living_situation <- function(value){
     value %in% c(10, 11, 21, 22, 23, 26) ~ value + 400,
     value %in% c(8, 9, 17, 24, 30, 37, 99) ~ value,
     is.na(value) ~ NA,
-    TRUE ~ 0
+    TRUE ~ 0 # 0 would mean something's wrong
   )
 }
 
@@ -21,16 +20,50 @@ ees_with_statuses <- Enrollment %>%
     EnrollmentID,
     ProjectType,
     RelationshipToHoH,
-    EntryDate,
     LivingSituation,
     LOSUnderThreshold,
-    PreviousStreetESSH
+    PreviousStreetESSH,
+    EntryDate,
+    MoveInDateAdjust,
+    ExitDate,
+    Destination
   ) %>%
   mutate(
-    LHAtEntry = if_else(
+    EntryStatusHomeless = if_else(
       ProjectType %in% c(lh_project_types) |
-        (TRUE & TRUE), 1, 0
+        (LivingSituation %in% c(homeless_livingsituation) & 
+           ProjectType != 12) |
+        (LivingSituation %in% c(not_homeless_livingsituation) & 
+           ProjectType != 12 &
+           LOSUnderThreshold == 1 &
+           PreviousStreetESSH == 1), 1, 0
     ),
+    HousedLeavers = if_else(
+      EntryStatusHomeless == 1 &
+        ProjectType %in% c(ph_project_types) &
+        !is.na(MoveInDateAdjust) &
+        MoveInDateAdjust >= meta_HUDCSV_Export_Start &
+        MoveInDateAdjust <= meta_HUDCSV_Export_End &
+        ProjectType != 12 &
+        Destination %in% c(perm_livingsituation) &
+        !is.na(ExitDate) &
+        ExitDate >= meta_HUDCSV_Export_Start &
+        ExitDate <= meta_HUDCSV_Export_End,
+      1, 0
+      ),
+    HousedAll = if_else(
+      EntryStatusHomeless == 1 &
+        ProjectType %in% c(ph_project_types) &
+        !is.na(MoveInDateAdjust) &
+        MoveInDateAdjust >= meta_HUDCSV_Export_Start &
+        MoveInDateAdjust <= meta_HUDCSV_Export_End &
+        ProjectType != 12 &
+        Destination %in% fy22_to_fy24_living_situation(c(perm_livingsituation)) &
+        (is.na(ExitDate) |
+           ExitDate >= meta_HUDCSV_Export_Start),
+      1, 0
+    ),
+    
     FY24Translation = fy22_to_fy24_living_situation(LivingSituation)
       
   )
