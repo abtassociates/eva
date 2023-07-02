88cfd63354f68eb27f03609a1aa137eb02f3cab7
Genelle Denzin <Genelle_Denzin@abtassoc.com>
2023-07-02 09:10:06 -0400

    moved fy22_to_fy24_living_situation function here so it is run before other scripts

--- helper_functions.R
+++ helper_functions.R
@@ -265,6 +265,22 @@ nice_names <- function(df){
   df
 }
 
+
+# Old to New Living SItuations --------------------------------------------
+
+fy22_to_fy24_living_situation <- function(value){
+  case_when(
+    value %in% c(3, 19, 20, 28, 31, 33, 34, 38, 39) ~ 435,
+    value %in% c(lh_livingsituation) ~ value + 100,
+    value %in% c(4, 5, 6, 7, 15, 25) ~ value + 200,
+    value %in% c(2, 12, 13, 14, 27, 29, 32, 35, 36) ~ value + 300,
+    value %in% c(10, 11, 21, 22, 23, 26) ~ value + 400,
+    value %in% c(8, 9, 17, 24, 30, 37, 99) ~ value,
+    is.na(value) ~ NA,
+    TRUE ~ 0 # 0 would mean something's wrong
+  )
+}
+
 #############################
 # SANDBOX
 #############################
