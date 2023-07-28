ace3eecc7c7b71f64b1848a3088ae6af7915975d
Genelle Denzin <Genelle_Denzin@abtassoc.com>
2023-07-28 10:08:43 -0400

    this isn't used anywhere

--- 02_export_dates.R
+++ 02_export_dates.R
@@ -10,4 +10,3 @@ meta_HUDCSV_Export_End <<- Export %>% pull(ExportEndDate)
 
 meta_HUDCSV_Export_Date <<- Export %>% pull(ExportDate)
 
-file_date_range <<- interval(meta_HUDCSV_Export_Start, meta_HUDCSV_Export_End)
