d55a179f31713f041ddba0929b6b49a0c2c30f0e
alex-silverman <alexsilverman96@gmail.com>
2023-10-15 14:35:56 -0400

    created shinytest2 version of the test-good script

--- tests/testthat/test-good.R
+++ tests/testthat/test-good.R
@@ -1,4 +1,5 @@
 library(shinytest2)
+source("../testing_functions.R", local = TRUE)
 
 test_that("{shinytest2} recording: test-good", {
     print("Running test-good")
@@ -9,9 +10,10 @@ test_that("{shinytest2} recording: test-good", {
         load_timeout=1e+05)
 app$set_inputs(Go_to_upload = "click")
 app$upload_file(imported = "../FY24-ICF-hashed-current-good.zip") # <-- This should be the path to the file, relative to the app's tests/shinytest directory
-app$find_element("button[data-dismiss='modal']")$click()
+# app$find_element("button[data-dismiss='modal']")$click()
+
 print("about to cusotm download")
-customDownload("downloadFileStructureAnalysis","File-Structure-Analysis-Download")
+customDownload(app, "downloadFileStructureAnalysis","File-Structure-Analysis-Download")
 app$expect_values()
 
 print("going to client counts")
