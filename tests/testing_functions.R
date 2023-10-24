ce11264afe1458ac41a1895fafd615439f057400
Alex Silverman <silvermana@aneprr-xip.ad.abt.local>
2023-10-24 14:53:51 -0400

    ensure snapshot data is not stored in an OS-specific folder, since devs could use different OSs from each other vs. GitHub Actions

--- tests/testing_functions.R
+++ tests/testing_functions.R
@@ -8,7 +8,7 @@ main_test_script <- function(test_script_name, test_dataset) {
     print(paste0("Running ",test_script_name))
     
     app <- AppDriver$new(
-      variant = platform_variant(), 
+      variant = platform_variant(os_name = FALSE), 
       name = test_script_name, 
       seed = 12345,
       load_timeout = 1e+05)
@@ -40,7 +40,7 @@ initially_invalid_test_script <- function(test_script_name, test_dataset) {
   print(paste0("Running ",test_script_name))
 
   app <- AppDriver$new(
-      variant = platform_variant(), 
+      variant = platform_variant(os_name = FALSE), 
       name = test_script_name, 
       seed = 12345,
       load_timeout = 1e+05)
