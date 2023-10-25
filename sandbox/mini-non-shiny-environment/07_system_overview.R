8946f8a7a074326ff4cc98e16a61d518099f9e8a
Genelle Denzin <Genelle_Denzin@abtassoc.com>
2023-10-25 13:05:03 -0400

    added more nuance to how to assign the CorrectedHoHs

--- sandbox/mini-non-shiny-environment/07_system_overview.R
+++ sandbox/mini-non-shiny-environment/07_system_overview.R
@@ -44,13 +44,28 @@ system_df_prep <- EnrollmentAdjust %>%
 # corrected hohs ----------------------------------------------------------
 
 hh_adjustments <- system_df_prep %>%
-  mutate(VeteranStatus = if_else(VeteranStatus == 1, 1, 0)) %>%
+  mutate(VeteranStatus = if_else(VeteranStatus == 1 &
+                                   !is.na(VeteranStatus), 1, 0),
+         HoHAlready = if_else(RelationshipToHoH == 1 &
+                                AgeAtEntry > 17, 1, 0)) %>%
   group_by(HouseholdID, ProjectID) %>%
-  arrange(desc(VeteranStatus), desc(AgeAtEntry)) %>%
+  arrange(desc(HoHAlready), desc(VeteranStatus), desc(AgeAtEntry), PersonalID) %>%
   mutate(Sequence = seq(n()),
          CorrectedHoH = if_else(Sequence == 1, 1, 0)) %>%
   ungroup() %>%
   select(EnrollmentID, CorrectedHoH)
+
+# keeps original HoH unless the HoH is younger than 18 or if there are mult hohs
+# if they are younger than 18, or if there are mult hohs, it will take the
+# veteran in the hh. if there are none or multiple veterans, it will take the
+# oldest. if they're the same veteran status and age, it will name the person
+# with the first PersonalID.
+# if the hh looks like: 
+# age 8 (hoh), age 40 (non-veteran), age 38 (veteran) ->  age 38 (veteran)
+# age 16 (hoh), age 8, age 3 -> hoh stays age 16 (bc they're the oldest)
+# age 42 (hoh), age 52 (veteran), age 5 -> hoh stays age 42 (hoh) (trusting the
+# decisions of the case manager, maybe the 42 year old was the eligible person)
+# age 53 (hoh), age 46 (hoh), age 17 -> only the age 53 and not the age 46
 
 # adding corrected hoh ----------------------------------------------------
 
