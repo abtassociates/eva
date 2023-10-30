99569b1cdd32dc7f557c9ce91f80f36101a4a1e3
Alex Silverman <silvermana@aneprr-wxo.ad.abt.local>
2023-10-29 22:41:04 -0400

    creating tests for RRH-SO projects with active inventory and Participation overlap

--- tests/update_test_good.R
+++ tests/update_test_good.R
@@ -47,20 +47,42 @@ Enrollment <- Enrollment %>%
   )
 
 
+# Participation overlap (checkIDs = 131) -------------------------------
+# this creates an "overlap" by duplicating a randomly selected existing record 
+# and modifying the start date to be 2 days before the original record's end date
+# to create a "gap", simply add/change the number to a positive number
+HMISParticipation <- map_dfr(c(-2), ~ {
+  HMISParticipation %>%
+    filter(HMISParticipationType == 1 & !is.na(HMISParticipationStatusEndDate)) %>%
+    sample_n(1) %>%
+    mutate(HMISParticipationStatusStartDate = HMISParticipationStatusEndDate + days(.x),
+           HMISParticipationStatusEndDate = NA,
+           HMISParticipationID = paste0(HMISParticipationID,abs(.x)))
+}) %>%
+  bind_rows(HMISParticipation)
 
-HMISParticipation <- HMISParticipation %>%
+# RRH-SO projects with active inventory (checkID = 132) ----------------------
+# this selects a random project with inventory and projecttype = 13
+random_project <- Inventory %>% 
+  filter(BedInventory > 0) %>%
+  semi_join(Project %>% filter(ProjectType == 13), by = "ProjectID") %>%
+  sample_n(1) %>%
+  pull(ProjectID)
+
+# Update RRHSubType for that project to 1
+Project <- Project %>%
   mutate(
-    HMISParticipationStatusStartDate = 
-      case_when(
-        row_number() %in% sample(
-          which(
-            HMISParticipationType == 1 & !is.na(HMISParticipationStatusEndDate)
-          ), 
-          1
-        ),
-        HMISParticipationStatusEndDate + days(6),
-        HMISParticipationStatusStartDate
-      ),
-    HMISParticipationStatusEndDate = NA,
-  )
+    RRHSubType = if_else(ProjectID == random_project, 1, RRHSubType),
+    RRHSOActivePeriod =
+      interval(OperatingStartDate,
+        coalesce(OperatingEndDate, meta_HUDCSV_Export_End))
+    )
 
+# finally, make sure inventory period overlaps project operating period
+Inventory <- Inventory %>% 
+  left_join(Project %>% 
+              select(ProjectID, OperatingStartDate, OperatingEndDate)
+            ,by = "ProjectID") %>%
+  mutate(InventoryStartDate = OperatingStartDate - days(1),
+         InventoryEndDate = OperatingEndDate + days(1)) %>%
+  select(-c(OperatingStartDate, OperatingEndDate))
