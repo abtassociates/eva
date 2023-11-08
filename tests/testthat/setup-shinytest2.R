88400e0cc13e28b6c9661e2fe47f0f1f3b044de4
Alex Silverman <alexsilverman96@gmail.com>
2023-11-08 11:52:56 -0500

    trying to call testing functions and creaet test datasets before loading environment in setup

--- tests/testthat/setup-shinytest2.R
+++ tests/testthat/setup-shinytest2.R
@@ -1,5 +1,6 @@
-# Load application support files into testing environment
-shinytest2::load_app_env()
-source("tests/testing_functions.R", local = TRUE)
+library(here)
+setwd(here())
+source(here("tests/testing_functions.R"), local = TRUE)
 # create the various test datasets (will be saved in a temp directory)
-source("tests/create_test_datasets.R", local = TRUE)
+source(here("tests/create_test_datasets.R"), local = TRUE)
+shinytest2::load_app_env()
