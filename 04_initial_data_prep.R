8152e7bc863c56fbf218a4675a5ecf1347674030
Alex Silverman <alexsilverman96@gmail.com>
2023-11-15 13:13:42 -0500

    don't add ExitAdjust to EnrollmentOutside. Otherwise, when we then join to enrollmentstaging, which already has it,

--- 04_initial_data_prep.R
+++ 04_initial_data_prep.R
@@ -153,7 +153,7 @@ EnrollmentOutside <- EnrollmentStaging %>%
   ungroup() %>%
   select(EnrollmentID, ProjectID, ProjectTimeID, ProjectType, EnrollmentDateRange,
          OperatingDateRange, ParticipatingDateRange, EnrollmentvParticipating,
-         EnrollmentvOperating, ExitAdjust)
+         EnrollmentvOperating)
 
 Enrollment <- EnrollmentStaging %>%
   left_join(EnrollmentOutside,
