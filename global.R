951571b05a4b253d9b139c938c9cd2e9498132b4
Alex Silverman <alexsilverman96@gmail.com>
2024-01-15 21:40:17 -0500

    added references to data.table and dtplyr which are used to do some data manipulations faster than dplyr

--- global.R
+++ global.R
@@ -15,6 +15,8 @@ library(janitor)
 library(shinyjs)
 library(shinydisconnect)
 library(here)
+library(dtplyr)
+library(data.table)
 
 options(shiny.maxRequestSize = 200000000) # <- about 200MB, aka 200*1024^2
 
