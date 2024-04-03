ad50a00b6eae04cd71edd53ac74e39e4cbb0d299
alex-silverman <alexsilverman96@gmail.com>
2024-04-02 20:40:25 -0400

    Update changelog.R to reflect loading spinners

--- changelog.R
+++ changelog.R
@@ -4,6 +4,9 @@ output$changelog <- renderTable({
     "04-01-2024", "Added 'Demo Mode', which allows users to play around with a 
     test dataset in Eva in order to understand how the tool works without having 
     to upload any data",
+
+     "04-01-2024", "Added loading spinners to make clear when something is loading
+     as opposed to frozen",
     
     "01-24-2024", "If a project has an Operating or Participating end date in 
     the future Eva will not flag these enrollments as Enrollment Crosses 
