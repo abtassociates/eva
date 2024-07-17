e26847d864776d96b60342e9fbf8be45fc7d48c6
Genelle Denzin <Genelle_Denzin@abtassoc.com>
2024-07-17 15:55:05 -0400

    remove browser (sorry!)

--- 07_system_overview.R
+++ 07_system_overview.R
@@ -746,7 +746,7 @@ clients_enrollments_reactive <- reactive({
 # get final people-level, inflow/outflow dataframe by joining the filtered 
 # enrollment and people dfs, as well as flagging their inflow and outflow types
 inflow_outflow_df <- reactive({
-  browser()
+  # browser()
   # add inflow type and active enrollment typed used for system overview plots
   universe <-
     clients_enrollments_reactive() %>%
