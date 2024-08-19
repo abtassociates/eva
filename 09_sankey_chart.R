463f50066863be1149cbf9ddf418674ab98d2e3d
alex-silverman <alexsilverman96@gmail.com>
2024-08-19 17:23:16 -0400

    wrapped all the 09 sankey chart data prep in a reactive, since it should react to changes in the filters. Also, pulling from the new sys_dF_universe() reactiveVal, which in turn contains the universe_ppl_flags reactive defined in 07. This is an enrollment-level dataframe, but with client-level flags needed to determine their sankey chart categorization

--- 09_sankey_chart.R
+++ 09_sankey_chart.R
@@ -1,46 +1,60 @@
 # The universe is anyone who was Housed or Homeless at Period Start
-plot_data <- sys_inflow_outflow_plot_data()() %>%
-  filter(InflowTypeDetail %in% c("Homeless", "Housed"))
+# We also need the latest exit for the folks in the Exited categories
+plot_data <- reactive({
+  plot_df <- sys_df_universe() %>%
+    filter(InflowTypeDetail == "Housed" | InflowTypeDetail == "Homeless") %>%
+    group_by(PersonalID) %>%
+    mutate(
+      lastExit = max(ExitAdjust) == ExitAdjust
+    ) %>%
+    filter(max(lastExit) == lastExit) %>%
+    ungroup() %>%
+    select(PersonalID, InflowTypeDetail, OutflowTypeDetail, lastExit, ExitAdjust, Destination) %>%
+    unique()
 
-startBind <- plot_data %>%
-  select(PersonalID, "Type" = InflowTypeDetail) %>%
-  mutate("Period" = "Begin")
-
-endBind <- plot_data %>%
-  mutate(
-    "Period" = "End",
-    "Type" = case_when(
-      unknown_at_end_client == TRUE ~ "Inactive", 
-      OutflowTypeDetail == "Housed" ~ "Enrolled, Housed",
-      OutflowTypeDetail == "Homeless" ~ "Enrolled, Homeless",
-      OutflowTypeDetail == "Exited to \nPermanent Destination" ~ "Exited, Permanent",
-      OutflowTypeDetail == "Exited to \nNon-Permanent Destination" ~ "Exited, Non-Permanent"
-    )
-  ) %>%
-  select(PersonalID, Period, Type)
+  startBind <- plot_df %>%
+    select(PersonalID, "Type" = InflowTypeDetail) %>%
+    mutate("Period" = "Begin")
   
+  endBind <- plot_df %>%
+    mutate(
+      "Period" = "End",
+      "Type" = case_when(
+        OutflowTypeDetail == "Housed" ~ "Enrolled, Housed",
+        OutflowTypeDetail == "Homeless" ~ "Enrolled, Homeless",
+        lastExit == TRUE & 
+          Destination %in% perm_destinations &
+          ExitAdjust <= ReportEnd() ~ "Exited, Permanent",
+        lastExit == TRUE & 
+          !(Destination %in% perm_destinations) &
+          ExitAdjust <= ReportEnd() ~ "Exited, Non-Permanent",
+        TRUE ~ OutflowTypeDetail
+      )
+    ) %>%
+    select(PersonalID, Period, Type)
   
-allBind <- rbind(startBind, endBind)
-
-#Create df with both Homeless and Housed at start
-d_hh <- data.frame(cbind(startBind$Type, endBind$Type))
-names(d_hh) <- c("Period Start", "Period End")
-
-#Basic alluvial chart - both homeless and housed at start
-allu <- d_hh %>%
-  group_by(d_hh$`Period Start`, d_hh$`Period End`) %>%
-  summarise(Freq = n())
-
-names(allu) <- c("Begin", "End", "freq")
-
-#Convert statuses as factors and re-order levels
-allu$Begin <- factor(allu$Begin, levels = c("Housed",
-                                            "Homeless"))
-
-allu$End <- factor(allu$End, levels = c("Exited, Permanent",
-                   "Enrolled, Housed",
-                   "Inactive",
-                   "Exited, Non-Permanent",
-                   "Enrolled, Homeless"))
-
-sankey_plot_data(allu)
+    
+  allBind <- rbind(startBind, endBind)
+  
+  #Create df with both Homeless and Housed at start
+  d_hh <- data.frame(cbind(startBind$Type, endBind$Type))
+  names(d_hh) <- c("Period Start", "Period End")
+  
+  #Basic alluvial chart - both homeless and housed at start
+  allu <- d_hh %>%
+    group_by(d_hh$`Period Start`, d_hh$`Period End`) %>%
+    summarise(Freq = n())
+  
+  names(allu) <- c("Begin", "End", "freq")
+  
+  #Convert statuses as factors and re-order levels
+  allu$Begin <- factor(allu$Begin, levels = c("Housed",
+                                              "Homeless"))
+  
+  allu$End <- factor(allu$End, levels = c("Exited, Permanent",
+                     "Enrolled, Housed",
+                     "Inactive",
+                     "Exited, Non-Permanent",
+                     "Enrolled, Homeless"))
+  allu
+})
