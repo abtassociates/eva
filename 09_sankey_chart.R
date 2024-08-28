94072ae17e2843369ece0b66367e76ef71019bfa
alex-silverman <alexsilverman96@gmail.com>
2024-08-28 16:51:10 -0400

    simplify the sankey chart to start with the waterfall chart data. We may need to roll this back, but after talking to victoria and Genelle it seems like the InflowType and OutflowType statuses are the same. The only difference right now might be how ties in latest exists (not crossing report end) are handled. But if they're handled differently, they shouldn't be, so we'll update the tie-breaking logic in the 07 code.

--- 09_sankey_chart.R
+++ 09_sankey_chart.R
@@ -1,61 +1,17 @@
 # The universe is anyone who was Housed or Homeless at Period Start
 # We also need the latest exit for the folks in the Exited categories
 plot_data <- reactive({
-  plot_df <- sys_df_universe() %>%
+  
+  plot_df <- sys_inflow_outflow_plot_data() %>%
     filter(InflowTypeDetail == "Housed" | InflowTypeDetail == "Homeless") %>%
-    group_by(PersonalID) %>%
-    filter(max(ExitAdjust) == ExitAdjust) %>%
-    ungroup() %>%
-    select(PersonalID, InflowTypeDetail, OutflowTypeDetail, Destination, EnrollmentID, ExitAdjust) %>%
-    unique()
-    
-  # if any enrollments share the same exit date, dedup them using the following logic
-    # 1. take permanent over temp over other
-    # 2. within a destination type (perm, temp, oother) take the lower destination value
-    # 3. within the same destination number, take the highest enrollment ID
-  if(any(duplicated(plot_df$PersonalID))) {
-    destination_categories <- c("Permanent" = 1, "Temp" = 2, "Other" = 3)
-    plot_df <- plot_df %>%
-      group_by(PersonalID) %>%
-      mutate(
-        DestinationCategory = factor(
-          case_when(
-            Destination %in% perm_livingsituation ~ 1,
-            Destination %in% c(
-              temp_livingsituation,
-              institutional_livingsituation,
-              homeless_livingsituation_incl_TH
-            ) ~ 2,
-            TRUE ~ 3
-          ), 
-          levels = destination_categories,
-          labels = names(destination_categories)
-        )
-      ) %>%
-      arrange(DestinationCategory, Destination, desc(EnrollmentID)) %>%
-      slice(1) %>%
-      ungroup()
-  }
 
   startBind <- plot_df %>%
     select(PersonalID, "Type" = InflowTypeDetail) %>%
     mutate("Period" = "Begin")
   
   endBind <- plot_df %>%
-    mutate(
-      "Period" = "End",
-      "Type" = case_when(
-        OutflowTypeDetail == "Housed" ~ "Enrolled, Housed",
-        OutflowTypeDetail == "Homeless" ~ "Enrolled, Homeless",
-        Destination %in% perm_livingsituation &
-          ExitAdjust <= ReportEnd() ~ "Exited, Permanent",
-        !(Destination %in% perm_livingsituation) &
-          ExitAdjust <= ReportEnd() ~ "Exited, Non-Permanent",
-        TRUE ~ OutflowTypeDetail
-      )
-    ) %>%
-    select(PersonalID, Period, Type)
-  
+    select(PersonalID, "Type" = OutflowTypeDetail) %>%
+    mutate("Period" = "End")
     
   allBind <- rbind(startBind, endBind)
   
