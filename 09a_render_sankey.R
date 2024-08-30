d1794e44fac0c76d0a677a0ae259d1b1c426a678
alex-silverman <alexsilverman96@gmail.com>
2024-08-30 13:38:03 -0400

    now that the output is defined in 09a, don't need a separate function

--- 09a_render_sankey.R
+++ 09a_render_sankey.R
@@ -5,20 +5,20 @@ output$sankey_filter_selections <- renderUI({
 
 output$sankey_ui_chart <- renderPlot({
   req(valid_file() == 1)
+  
+  plot_data <- sankey_plot_data()
+  
   validate(
     need(
-      sum(sankey_plot_data()$freq) > 0, 
+      sum(plot_data$freq) > 0, 
       message = paste0("No data to show.")
     ),
     need(
-      sum(sankey_plot_data()$freq) > 10,
+      sum(plot_data$freq) > 10,
       message = paste0("Not enough data to show.")
     )
   )
-  renderSankeyChart(sankey_plot_data())
-})
-
-renderSankeyChart <- function(plot_data) {
+  
   begin_labels <- plot_data %>%
     group_by(Begin) %>%
     summarize(freq = sum(freq)) %>%
@@ -61,94 +61,71 @@ renderSankeyChart <- function(plot_data) {
     "Enrolled, Housed" = "#16697a"
   )
 
-  return(
-    ggplot(
-      data = plot_data,
-      aes(axis1 = Begin, axis2 = End, y = freq)
-    ) +
-    geom_alluvium(aes(fill = End, colour = End), reverse = TRUE) +
-    geom_stratum(aes(fill = End), reverse = TRUE) +
-    
-    # construct the Begin bars
-    geom_rect(
-      aes(
-        fill = Begin,
-        xmin = 0.83,
-        xmax = 1.17,
-        ymin = ystart,
-        ymax = yend
-      )
-    ) +
-        
-    #Color for End stratum and alluvial flows
-    scale_fill_manual(values = colors) +
-      
-    #Color for alluvial flow borders
-    scale_color_manual(values = colors) +
-
-    # Numbers in bars
-    geom_text(stat = "stratum",
-              aes(label = after_stat(count)),
-              size = 5,
-              ) +
-    
-    # Bar (Text) Labels
-    geom_text(
-      data = begin_labels,
-      aes(x = 1, y = label_pos, label = Begin), 
-      hjust = 1,
-      nudge_x = -0.2,
-      size = 4
-    ) +
-    geom_text(
-      data = end_labels,
-      aes(x = 1, y = label_pos, label = End), 
-      hjust = 0,
-      nudge_x = 1.2,
-      size = 4
-    ) +
-    
-    # X Axis Labels
-    scale_x_discrete(label = c("Period Start", "Period End"),
-                     limits = c("Period Start", "Period End"),
-                     expand = c(0.5, 0.5)) +
-    
-    # Total People
-    annotate(
-      geom = "text",
-      x = 1.5,
-      y = max(plot_data$yend) * 1.1,
-      size = 16/.pt,
-      label = sys_total_count_display(sum(plot_data$freq))
-    ) +
-      
-    # remove legend, axis sizing
-    theme_void() +
-    theme(legend.position = "none",
-         axis.text.x = element_text(color = "black", size = 17, vjust = 2.5))
-  )
-}
-
-sys_export_summary_initial_df <- function() {
-  return(data.frame(
-    Chart = c(
-      "Start Date",
-      "End Date",
-      "Methodology Type",
-      "Household Type",
-      "Level of Detail",
-      "Project Type"
-    ),
-    Value = c(
-      strftime(ReportStart(), "%m/%d/%y"),
-      strftime(ReportEnd(), "%m/%d/%y"),
-      getNameByValue(syso_methodology_types, input$methodology_type),
-      getNameByValue(syso_hh_types, input$syso_hh_type),
-      getNameByValue(syso_level_of_detail, input$syso_level_of_detail),
-      getNameByValue(syso_project_types, input$syso_project_type)
+  ggplot(
+    data = plot_data,
+    aes(axis1 = Begin, axis2 = End, y = freq)
+  ) +
+  geom_alluvium(aes(fill = End, colour = End), reverse = TRUE) +
+  geom_stratum(aes(fill = End), reverse = TRUE) +
+  
+  # construct the Begin bars
+  geom_rect(
+    aes(
+      fill = Begin,
+      xmin = 0.83,
+      xmax = 1.17,
+      ymin = ystart,
+      ymax = yend
     )
-  ))
-}
+  ) +
+      
+  #Color for End stratum and alluvial flows
+  scale_fill_manual(values = colors) +
+    
+  #Color for alluvial flow borders
+  scale_color_manual(values = colors) +
+
+  # Numbers in bars
+  geom_text(stat = "stratum",
+            aes(label = after_stat(count)),
+            size = 5,
+            ) +
+  
+  # Bar (Text) Labels
+  geom_text(
+    data = begin_labels,
+    aes(x = 1, y = label_pos, label = Begin), 
+    hjust = 1,
+    nudge_x = -0.2,
+    size = 4
+  ) +
+  geom_text(
+    data = end_labels,
+    aes(x = 1, y = label_pos, label = End), 
+    hjust = 0,
+    nudge_x = 1.2,
+    size = 4
+  ) +
+  
+  # X Axis Labels
+  scale_x_discrete(label = c("Period Start", "Period End"),
+                   limits = c("Period Start", "Period End"),
+                   expand = c(0.5, 0.5)) +
+  
+  # Total People
+  annotate(
+    geom = "text",
+    x = 1.5,
+    y = max(plot_data$yend) * 1.1,
+    size = 16/.pt,
+    label = sys_total_count_display(sum(plot_data$freq))
+  ) +
+    
+  # remove legend, axis sizing
+  theme_void() +
+  theme(legend.position = "none",
+       axis.text.x = element_text(color = "black", size = 17, vjust = 2.5))
+})
 
 sys_export_filter_selections <- function() {
   return(tibble(
