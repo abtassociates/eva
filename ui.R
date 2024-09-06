2cd8bb64b8a0924d1521ac9521c78ffebc03642a
alex-silverman <alexsilverman96@gmail.com>
2024-09-06 16:16:02 -0400

    add PPT download buttons in the UI

--- ui.R
+++ ui.R
@@ -1160,7 +1160,8 @@ dashboardPage(
                                 </table>")
                   )
                 ),
-                downloadButton("sys_inflow_outflow_download_btn", "Download")
+                downloadButton("sys_inflow_outflow_download_btn", "Download"),
+                downloadButton("sys_inflow_outflow_download_btn_ppt", "Download PPT")
               ),
               tabPanel(
                 id = "syso_systemstatus",
@@ -1253,6 +1254,7 @@ dashboardPage(
                   )
                 ),
                 downloadButton("sys_status_download_btn", "Download"),
+                downloadButton("sys_status_download_btn_ppt", "Download PPT"),
                 width = 12
               ),
               tabPanel(
