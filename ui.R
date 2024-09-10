4bce6b1f7a173ec7534b3113b4b35ac18883184d
alex-silverman <alexsilverman96@gmail.com>
2024-09-10 11:30:57 -0400

    set timeout to 15 mins, rather than 10, as requested by Jesse

--- ui.R
+++ ui.R
@@ -79,7 +79,7 @@ dashboardPage(
       tags$link(rel = "stylesheet", type = "text/css", href = "custom.css"),
       tags$html(lang="en"), #Added as WAVE fix but not considered ideal
       tags$script(HTML("function idleTimer() {
-          var timeoutTime = 600000;
+          var timeoutTime = 900000;
           var t = setTimeout(logout, timeoutTime);
           window.onmousemove = resetTimer; // catches mouse movements
           window.onmousedown = resetTimer; // catches mouse movements
