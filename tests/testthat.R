cc5433ac58d678e4266e87eeb5f9e9084a27c921
alex-silverman <alexsilverman96@gmail.com>
2024-09-19 12:03:53 -0400

    updated snapshots

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,7 +1,9 @@
 library(here)
 library(shinytest2)
-unlink(here("tests/temp"), recursive = TRUE)
+
 # create reduced-sized versions of initially invalid test datasets
+unlink(here("tests/temp"), recursive = TRUE)
 source(here("tests/create_test_datasets.R"), local = TRUE)
+
 test_app(".")
 # testthat::snapshot_review()
