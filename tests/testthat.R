bbb5df959dd0bf6c0ceced1beef69cdf8d491111
alex-silverman <alexsilverman96@gmail.com>
2024-09-19 09:19:11 -0400

    move the line that creates the test data to testthat.R so it can easily be commented out for testing/debugging

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,5 +1,7 @@
 library(here)
 library(shinytest2)
 unlink(here("tests/temp"), recursive = TRUE)
+# create reduced-sized versions of initially invalid test datasets
+source(here("tests/create_test_datasets.R"), local = TRUE)
 test_app(".")
 # testthat::snapshot_review()
--- tests/testthat/setup-shinytest2.R
+++ tests/testthat/setup-shinytest2.R
@@ -1,5 +1,2 @@
-library(here)
 source(here("tests/testing_functions.R"), local = TRUE)
-# create reduced-sized versions of initially invalid test datasets
-source(here("tests/create_test_datasets.R"), local = TRUE)
 shinytest2::load_app_env()
