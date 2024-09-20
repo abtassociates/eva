c00c1a0457d9e4cd4b3405ba2d6091c425174479
alex-silverman <alexsilverman96@gmail.com>
2024-09-20 08:46:33 -0400

    moving setup stuff back to setup-shinytest2.R since the github action doesn't call the entire testthat.R script, only runs test_app()

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,9 +1,3 @@
-library(here)
 library(shinytest2)
-
-# create reduced-sized versions of initially invalid test datasets
-unlink(here("tests/temp"), recursive = TRUE)
-source(here("tests/create_test_datasets.R"), local = TRUE)
-
-test_app(".")
+test_app(".") # example filter: filter="main-valid"
 # testthat::snapshot_review()
--- tests/testthat/setup-shinytest2.R
+++ tests/testthat/setup-shinytest2.R
@@ -1,2 +1,8 @@
+library(here)
+
+# create reduced-sized versions of initially invalid test datasets
+unlink(here("tests/temp"), recursive = TRUE)
+source(here("tests/create_test_datasets.R"), local = TRUE)
+
 source(here("tests/testing_functions.R"), local = TRUE)
 shinytest2::load_app_env()
