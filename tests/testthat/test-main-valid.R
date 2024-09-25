49879b2bae7b275c484a100dfcb28bd213e7e532
alex-silverman <alexsilverman96@gmail.com>
2024-09-25 08:57:29 -0400

    remove successful upload modal. obscures rest picture. should only change png files.

--- tests/testthat/test-main-valid.R
+++ tests/testthat/test-main-valid.R
@@ -18,8 +18,8 @@ test_that(paste0("{shinytest2} recording: ",test_script_name), {
   app$upload_file(imported = paste0(here(test_dataset_folder),test_dataset))
   
   print(paste0("Just uploaded in ",test_script_name))
-  
   app$wait_for_idle(timeout = 1e+06)
+  app$click(selector="#shiny-modal")
   app$expect_values(name="just-uploaded")
 
   app$set_inputs(sidebarmenuid = "tabClientCount")
