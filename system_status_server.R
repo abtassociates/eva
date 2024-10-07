c7f1884016504b5a96c9cc98eb9e9c3e0462c369
alex-silverman <alexsilverman96@gmail.com>
2024-10-07 17:06:38 -0400

    undoing the addition of logmetadata. it's already happening in the code that creates the metadata/summary tab in the xlsx downloads

--- system_status_server.R
+++ system_status_server.R
@@ -190,11 +190,6 @@ output$sys_status_download_btn <- downloadHandler(
       col_names = TRUE
     )
 
-    logMetadata(paste0(
-      "Downloaded Sys Status Report",
-      if_else(isTruthy(input$in_demo_mode), " - DEMO MODE", "")
-    ))
-
     exportTestValues(sys_status_report = sankey_plot_data())
   }
 )
