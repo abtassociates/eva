8441865ea9a75a6e5ec576e87827475ca4eb6a59
alex-silverman <alexsilverman96@gmail.com>
2024-11-12 15:48:34 -0500

    add comment to show how to view diff of specific file

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,3 +1,3 @@
 library(shinytest2)
 test_app(".") # example filter: filter="main-valid"
-# testthat::snapshot_review()
+# testthat::snapshot_review()# testthat::snapshot_review(files="main-valid/test-main-valid-exportTestValues.json")
