2e54838597c372d0cda4441212659c8723e5e6c7
alex-silverman <alexsilverman96@gmail.com>
2024-11-22 10:54:46 -0500

    move the initially_invalid_test_script to its own function

--- tests/testthat/setup-shinytest2.R
+++ tests/testthat/setup-shinytest2.R
@@ -5,4 +5,5 @@ unlink(here("tests/temp"), recursive = TRUE)
 source(here("tests/create_test_datasets.R"), local = TRUE)
 
 source(here("tests/testing_functions.R"), local = TRUE)
+source(here("tests/initially_invalid_test_script.R"), local = TRUE)
 shinytest2::load_app_env()
