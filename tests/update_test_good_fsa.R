9cbeefc89cdc0caa1fa582b42953c62acd501b2e
alex-silverman <alexsilverman96@gmail.com>
2024-12-02 11:16:28 -0500

    add fake data for the fsa check

--- tests/update_test_good_fsa.R
+++ tests/update_test_good_fsa.R
@@ -18,4 +18,11 @@ reduced_data_fsa$Client <- reduced_data_fsa$Client %>% select(-BlackAfAmerican)
 # incorrect/misspelled column (check 12)
 reduced_data_fsa$Client <- reduced_data_fsa$Client %>% rename(Transgenderr = Transgender)
 
+# impermissible characters
+reduced_data_fsa$Project$ProjectName <- paste0(reduced_data_fsa$Project$ProjectName, "[")
+reduced_data_fsa$Project$ProjectCommonName <- paste0("“", reduced_data_fsa$Project$ProjectCommonName, "”")
+reduced_data_fsa$Organization$OrganizationCommonName <- replace_char_at(reduced_data_fsa$Organization$OrganizationName, 4, "ñ")
+reduced_data_fsa$Organization$OrganizationCommonName <- replace_char_at(reduced_data_fsa$Organization$OrganizationCommonName, 5, "😊")
+reduced_data_fsa$Organization$OrganizationName <- paste0(reduced_data_fsa$Organization$OrganizationName, "ع")
+
 ## add more checks here
