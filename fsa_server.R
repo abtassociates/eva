1a2c7f62cfcdbd3905786c47ccbf70afb9476c44
alex-silverman <alexsilverman96@gmail.com>
2024-12-13 14:11:12 -0500

    refactor the code to detect non UTF-8 characters. Major changes: - using purrr to do fast looping - detecting encoding with readr and using iconv to convert to UTF-8, based on most likely encoding, so that the printed characters are the actual characters - modified the export to be File, Location, and Text, where the text includes the full cell text. This is easier and faster than extracting the specific problem characters

--- fsa_server.R
+++ fsa_server.R
@@ -11,65 +11,35 @@ fread_type_mapping <- list(
 )
 
 # Function to detect non-UTF-8 characters and brackets
-detect_problematic_and_bracket_characters <- function(dt, file) {
-  problematic_characters <- list()
-  for (col in names(dt)) {
-    if (is.character(dt[[col]])) {
-      # Detect non-UTF-8 characters
-      invalid_utf8_rows <- which(!stringi::stri_enc_isutf8(dt[[col]]))
-      if (length(invalid_utf8_rows) > 0) {
-        problematic_characters$utf8_issues <- lapply(invalid_utf8_rows, function(row) {
-          list(
-            row = row, 
-            col = col,
-            chars= iconv(
-              unlist(stringi::stri_extract_all_regex(dt[[col]][row], "[\x80-\xFF]")),
-              from = "WINDOWS-1252",
-              to = "UTF-8"
-            )
-          )
-        })
-      }
-      
-      # Detect brackets
-      bracket_rows <- stringi::stri_extract_all_regex(dt[[col]], bracket_regex, omit_no_match = TRUE)
-      if (any(sapply(bracket_rows, function(x) length(x) > 0))) {
-        problematic_characters$brackets <- Filter(Negate(is.null), lapply(
-            seq_along(bracket_rows), 
-            function(i) {
-              if(length(bracket_rows[[i]] > 0)) list(
-                row = i, 
-                col = col,
-                chars = unlist(bracket_rows[[i]])
-              )
-            }
-          )
+detect_non_utf8_and_bracket_characters <- function(dt, file, encoding) {
+  # Vectorized detection for non-UTF-8 and bracket issues
+  # Identify character columns only
+  char_cols <- names(dt)[sapply(dt, is.character)]
+
+  # Create a list of problematic entries
+  problematic_characters <- purrr::map_dfr(char_cols, function(col) {
+    non_utf8_rows <- !stringi::stri_enc_isutf8(dt[[col]])
+    bracket_rows <- stringi::stri_extract_all_regex(dt[[col]], bracket_regex, omit_no_match = TRUE) %>%
+      purrr::map_lgl( ~ length(.) > 0) # Check if any match
+    
+    invalid_rows <- non_utf8_rows | bracket_rows
+    
+    # Create tibbles for invalid rows to store the problematic text and cell location
+    if (any(invalid_rows)) {
+      invalid_row_indices <- which(invalid_rows)
+      tibble::tibble(
+        File = file,
+        Location = paste0("column ", col, ", row ", invalid_row_indices),
+        Text = iconv(
+          dt[[col]][invalid_row_indices],
+          from = toupper(encoding),
+          to = "UTF-8",
         )
-      }
+      )
     }
-  }
-  if(length(problematic_characters) > 0) {
-    details <- data.frame()
-    for (issue_type in names(problematic_characters)) {
-      for (x in problematic_characters[[issue_type]]) {
-        details <- rbind(
-          details,
-          data.frame(
-            File = file,
-            Detail = paste0(
-              "Found impermissible character(s) in ", 
-              file, ".csv, ", 
-              "column ", x$col, 
-              ", line ", x$row, 
-              ": ", paste(x$chars, collapse=", ")
-            ),
-            stringsAsFactors = FALSE
-          )
-        )
-      }
-    }
-    return(details)
-  }
+  })
+  
+  return(problematic_characters)
 }
 
 non_utf8_files_detail <- reactive({
@@ -87,34 +57,31 @@ non_utf8_files_detail <- reactive({
     message = "Downloading Impermissible Character Export...", 
     value = 0,
     {
-      # Initialize an empty list to store the results
-      files_with_non_utf8 <- list()
-      
-      fnum <- length(file_list)
-      files_with_non_utf8 <- lapply(file_list, function(file) {
+      files_with_non_utf8 <- purrr::map_dfr(file_list, function(file) {
         # Import original data, since object names have been overwritten 
         # with windows-1252 chars removed
         filename <- utils::unzip(zipfile = input$imported$datapath, files = glue("{file}.csv"))
         dt <- fread(
           here(filename),
-          colClasses = unname(col_types[[file]]),
-          na.strings = "NA"
+          colClasses = unname(col_types[[file]])
         )
-        file.remove(filename)
         
-        incProgress(1/fnum)
+        incProgress(1/length(file_list))
+        
+        possible_encodings <- readr::guess_encoding(filename)
+        most_likely_encoding <- possible_encodings$encoding[
+          which.max(possible_encodings$confidence)
+        ]
+        
+        file.remove(filename) # remove the csv
         
         return(
-          detect_problematic_and_bracket_characters(dt, file)
+          detect_non_utf8_and_bracket_characters(dt, file, most_likely_encoding)
         )
       })
     }
   )
   
   # Combine all data frames in the list into one data frame
-  return(
-    bind_rows(files_with_non_utf8) %>% 
-      merge_check_info(checkIDs = 134) %>%
-      select(all_of(issue_display_cols))
-  )
+  return(files_with_non_utf8)
 })
