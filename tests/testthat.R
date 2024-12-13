7e92c7c06fba8d2d7db5a030e29fc9eb55580356
alex-silverman <alexsilverman96@gmail.com>
2024-12-13 15:51:29 -0500

    automatically using set_max_fails to up the number of accepted failures. Otherwise, it will delete snapshots and we'll have to re-run

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,6 +1,6 @@
 library(shinytest2)
+testthat::set_max_fails(100)
 test_app(".") # example filter: filter="main-valid"
-# testthat::set_max_fails(100) --> in case you expect all tests to fail, you can make sure it gets through all of them
 # testthat::snapshot_review()
 # testthat::snapshot_review(files="main-valid/test-main-valid-exportTestValues.json")
 # testthat::snapshot_accept(files="fsa/test-fsa-001.json")
