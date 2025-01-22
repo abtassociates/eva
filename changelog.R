0730a1d8e2be9e414ebe264195bdb207de1b9c9a
Alex Silverman <alexsilverman96@gmail.com>
2025-01-22 11:27:49 -0500

    update changelog

--- changelog.R
+++ changelog.R
@@ -2,10 +2,13 @@ output$changelog <- renderDT({
   tribble(
     ~ Date,
     ~ Change,
-    "1-20-2024",
+    "1-23-2024",
     "<b>New Features</b> <br>
       - Added client-level export to the System Performance tab to provide transparency
-      in the charts",
+      in the charts<br>
+     <b>Bug Fixes:</b> <br>
+      - Fixed display of missing geography and address warnings for PDDE.<br>
+      - Only flag Subtype 2 for project type 13 (RRH) for Active Inventory PDDE check.",
     "12-31-2024",
     "<b>New features:</b> <br>
       - Updated demo.zip to capture new overlaps.<br>
