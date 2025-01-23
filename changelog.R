57ae777e68432743b89316f4364f2a9d05a5b316
zabalad <diana_zabala@abtassoc.com>
2025-01-23 14:25:49 -0500

    Revised changelog to reflect update to inventory pdde check

--- changelog.R
+++ changelog.R
@@ -8,7 +8,7 @@ output$changelog <- renderDT({
       in the charts<br>
      <b>Bug Fixes:</b> <br>
       - Fixed display of missing geography and address warnings for PDDE.<br>
-      - Only flag Subtype 2 for project type 13 (RRH) for Active Inventory PDDE check.",
+      - Only flag residential projects for Active Inventory PDDE check.",
     "12-31-2024",
     "<b>New features:</b> <br>
       - Updated demo.zip to capture new overlaps.<br>
