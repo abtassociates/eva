ff5536aef5ce884ef7b7c5f9859f4d76e6623539
Alex Silverman <silvermana@aneprrstu-01.ad.abt.local>
2025-01-27 17:26:52 -0500

    revised way to check for brackets. should be faster and works with data.table

--- fsa_server.R
+++ fsa_server.R
@@ -8,17 +8,19 @@ detect_bracket_characters <- function(dt, file, encoding) {
 
   # Create a list of problematic entries
   problematic_characters <- purrr::map_dfr(char_cols, function(col) {
-    bracket_rows <- stringi::stri_extract_all_regex(dt[[col]], bracket_regex, omit_no_match = TRUE) %>%
-      purrr::map_lgl( ~ length(.) > 0) # Check if any match
+    if(all(is.na(dt[[col]]))) return(tibble::tibble())
+    bracket_rows <- stringi::stri_detect_regex(dt[[col]], bracket_regex)
     
     # Create tibbles for invalid rows to store the problematic text and cell location
-    if (any(bracket_rows)) {
+    if (any(bracket_rows, na.rm = TRUE)) {
       row_indices <- which(bracket_rows)
       tibble::tibble(
         File = file,
         Location = paste0("column ", col, ", row ", row_indices),
         Text = dt[[col]][row_indices]
       )
+    } else {
+      tibble::tibble()
     }
   })
   
