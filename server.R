c59de75ee9b4e77ece2f77a6bc2db48b918e777d
Alex Silverman <silvermana@aneprrstu-01.ad.abt.local>
2025-01-31 22:10:09 -0500

    logging file sizes/memory: - zip file size - unpacked size - memory after processing

--- server.R
+++ server.R
@@ -149,6 +149,8 @@ function(input, output, session) {
 
     if(initially_valid_import() == 1) {
 
+      logMetadata(paste0("Unpacked file size, all files (KB) = ", sum(zipContents$Length) / 1024))
+      logMetadata(paste0("Unpacked file size, main files (KB) = ", sum(zipContents[sub(".csv$","", zipContents$Name) %in% unique(cols_and_data_types$File), ]$Length) / 1024))
       hide('imported_progress')
 
       setProgress(detail = "Unzipping...", value = .10)
@@ -247,6 +249,7 @@ function(input, output, session) {
           logToConsole("Done processing")
           
           
+          logMetadata(paste0("Memory used after processing: ", pryr::mem_used()))
           logToConsole("Upload processing complete")
           
           if(nrow(file_structure_analysis_main()) > 0) {
@@ -358,6 +361,7 @@ function(input, output, session) {
   }
   
   observeEvent(input$imported, {
+    logMetadata(paste0("Beginning upload. File size (KB) = ", input$imported$size))
     process_upload(input$imported$name, input$imported$datapath)
   }, ignoreInit = TRUE)
   
