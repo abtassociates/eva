0d35e1b53f2da5ff7e8ed7bb5b2b12d23269e56b
Alex Silverman <silvermana@aneprrstu-01.ad.abt.local>
2025-02-03 11:34:38 -0500

    refactoring the code a bit to exit early, instead of chunked if statements.

--- 03_file_structure_analysis.R
+++ 03_file_structure_analysis.R
@@ -18,14 +18,20 @@ high_priority_columns <- cols_and_data_types %>%
 # Brackets --------------------------------------------------------------
 files_with_brackets <- data.frame()
 for(file in unique(cols_and_data_types$File)) {
-  if (isTruthy(any(str_detect(as.matrix(get(file)), bracket_regex)))) {
+  m <- get(file)  # Load dataset
+  char_cols <- which(sapply(m, is.character))
+  if (length(char_cols) == 0) next
+  
+  m_mat <- as.matrix(m[, char_cols, drop = FALSE])  # Convert relevant columns to matrix
+  if (any(grepl(bracket_regex, m_mat, perl=TRUE), na.rm=TRUE)) {
     files_with_brackets <- data.frame(
-        Detail = str_squish("Found one or more brackets in your HMIS CSV Export. 
-          See Impermissible Character Detail export for the precise location of 
-          these characters.")
-      ) %>%
-        merge_check_info(checkIDs = 134) %>%
-        select(all_of(issue_display_cols))
+      File = file,
+      Detail = str_squish("Found one or more brackets in your HMIS CSV Export. 
+                See Impermissible Character Detail export for the precise location 
+                of these characters.")
+    ) %>%
+      merge_check_info(checkIDs = 134) %>%
+      select(all_of(issue_display_cols))
     break
   }
 }
--- fsa_server.R
+++ fsa_server.R
@@ -15,9 +15,10 @@ detect_bracket_characters <- function(dt, file) {
   # Vectorized detection for non-UTF-8 and bracket issues
   # Identify character columns only
   char_cols <- names(dt)[sapply(dt, is.character)]
-
+  if (length(char_cols) == 0) next
+  
   results <- lapply(char_cols, function(col) {
-    bracket_rows <- which(stringi::stri_detect_regex(dt[[col]], bracket_regex, na.rm = TRUE))
+    bracket_rows <- which(grepl(bracket_regex, dt[[col]], perl=TRUE))
     if (length(bracket_rows) == 0) return(NULL)
     
     data.table(
@@ -30,46 +31,17 @@ detect_bracket_characters <- function(dt, file) {
   rbindlist(results, use.names = TRUE, fill = TRUE)
 }
 
-bracket_files_detail <- reactive({
-  req(input$imported)
+bracket_files_detail <- function() {
   file_list <- unique(cols_and_data_types$File)
-  
-  col_types <- lapply(file_list, function(file) {
-    types_str <- get_col_types(input$imported$datapath, file)
-    types <- strsplit(types_str, "")[[1]]
-    sapply(types, function(type) fread_type_mapping[[type]])
-  })
-  names(col_types) <- file_list
-  
+
   withProgress(
-    message = "Downloading Impermissible Character Export...", 
-    value = 0,
-    {
-      files_with_brackets <- purrr::map_dfr(file_list, function(file) {
-        # Import original data, since object names have been overwritten 
-        # with windows-1252 chars removed
-        filename <- utils::unzip(zipfile = input$imported$datapath, files = glue("{file}.csv"))
-        dt <- fread(
-          here(filename),
-          colClasses = unname(col_types[[file]])
-        )
-        
-        incProgress(1/length(file_list))
-        
-        possible_encodings <- readr::guess_encoding(filename)
-        most_likely_encoding <- possible_encodings$encoding[
-          which.max(possible_encodings$confidence)
-        ]
-        
-        file.remove(filename) # remove the csv
-        
-        return(
-          detect_bracket_characters(dt, file, most_likely_encoding)
-        )
-      })
-    }
-  )
-  
-  # Combine all data frames in the list into one data frame
-  return(files_with_brackets)
-})
+    message = "Downloading Impermissible Character Export...", {
+    results <- lapply(file_list, function(file) {
+      dt <- importFile(upload_filepath = input$imported$datapath, csvFile = file)  # Load file
+      incProgress(1 / length(file_list))
+      detect_bracket_characters(dt, file)
+    })
+    
+    rbindlist(results, use.names = TRUE, fill = TRUE)
+  })
+}
