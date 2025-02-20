ced778138a6156cdc43fb5af0f640c375e57c049
Alex Silverman <silvermana@aneprrstu-01.ad.abt.local>
2025-02-20 15:53:37 -0500

    - removing old deletion of Export.csv, since it's now part of importFile function. - make sure to log to console each dataset that is imported. Print is not sufficient

--- 00_initially_valid_import.R
+++ 00_initially_valid_import.R
@@ -26,7 +26,6 @@ show_invalid_popup <- function(popupText = NULL, issueID, title) {
 
 hasGT1ExportRow <- function() {
   Export(importFile(upload_filepath, "Export"))
-  file.remove("Export.csv")
   
   # this is the soonest we can log the session data, with 
   # the export info, since this is the first time we import the Export.csv file
