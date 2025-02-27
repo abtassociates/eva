da346ce681f8135f8cb8d71f9c3d464c4e7fff18
zabalad <diana_zabala@abtassoc.com>
2025-02-27 12:58:36 -0500

    Adding "DEVOPS - " prefix to applicable new logMetadata calls

--- server.R
+++ server.R
@@ -149,8 +149,8 @@ function(input, output, session) {
 
     if(initially_valid_import() == 1) {
 
-      logMetadata(paste0("Unpacked file size, all files (KB) = ", sum(zipContents$Length) / 1024))
-      logMetadata(paste0("Unpacked file size, main files (KB) = ", sum(zipContents[sub(".csv$","", zipContents$Name) %in% unique(cols_and_data_types$File), ]$Length) / 1024))
+      logMetadata(paste0("DEVOPS - Unpacked file size, all files (KB) = ", sum(zipContents$Length) / 1024))
+      logMetadata(paste0("DEVOPS - Unpacked file size, main files (KB) = ", sum(zipContents[sub(".csv$","", zipContents$Name) %in% unique(cols_and_data_types$File), ]$Length) / 1024))
       hide('imported_progress')
 
       setProgress(detail = "Unzipping...", value = .10)
@@ -249,7 +249,7 @@ function(input, output, session) {
           logToConsole("Done processing")
           
           
-          logMetadata(paste0("Memory used after processing: ", pryr::mem_used()))
+          logMetadata(paste0("DEVOPS - Memory used after processing: ", pryr::mem_used()))
           logToConsole("Upload processing complete")
           
           if(nrow(file_structure_analysis_main()) > 0) {
@@ -361,7 +361,7 @@ function(input, output, session) {
   }
   
   observeEvent(input$imported, {
-    logMetadata(paste0("Beginning upload. File size (KB) = ", input$imported$size))
+    logMetadata(paste0("DEVOPS - Beginning upload. File size (KB) = ", input$imported$size))
     process_upload(input$imported$name, input$imported$datapath)
   }, ignoreInit = TRUE)
   
