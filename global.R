d1dada591d5948c9683f16c02c1a197e1b615872
alex-silverman <alexsilverman96@gmail.com>
2025-03-29 16:59:58 -0400

    use shinydashboardPlus2
    It's an updated fork of shinydashboard and supports Bootstrap 5, whereas shinydashboard only supports Bootstrap 3. And shinydashboardPlus2 is backwards compatible. We need Bootstrap 5 to pass the Invicti security issue of out-of-date bootstrap

--- global.R
+++ global.R
@@ -3,7 +3,7 @@ Sys.setenv(TZ = "America/New_York")
 
 library(tidyverse)
 library(lubridate)
-library(shinydashboard)
+library(shinydashboardPlus2)
 library(shiny)
 library(shinyWidgets)
 library(scales)
