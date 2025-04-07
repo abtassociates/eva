ace49a03209c5b9e30e248afa17148067da0af08
Alex Silverman <silvermana@aneprrstu-01.ad.abt.local>
2025-04-07 10:59:21 -0400

    remove the commented out mirai stuff for the system overview loop. It doesn't seem to be the right way to go and can't get it to work easily. Also, move everything from system_overview_data_prep_server to system_overview_server. It's unintuitive to split them out

--- system_overview_server.R
+++ system_overview_server.R
@@ -390,7 +390,137 @@ observeEvent({
 
 source("client_level_export_server.R", local=TRUE)
 
-source("system_overview_data_prep_server.R", local=TRUE)
+# Get period report_dates --------------------------------------------
+get_months_in_report_period <- function() {
+  seq.Date(from = session$userData$ReportStart, to = session$userData$ReportEnd, by = "months")
+} 
+get_report_dates <- function() {
+  months_in_report_period <- get_months_in_report_period()
+  c(
+    list("Full" = c(session$userData$ReportStart, session$userData$ReportEnd)),
+    setNames(
+      lapply(months_in_report_period, function(d) {
+        c(d, ceiling_date(d, "month") - days(1))
+      }),
+      months_in_report_period
+    )
+  )
+}
+
+# Cache management for period-specific universe_ppl_flag datasets --------------
+# Check cache size - this keeps the cache manageable
+check_cache_size <- function(cache, max_size_mb = 100) {
+  cache_size <- utils::object.size(cache) / 1024^2  # Convert to MB
+  if (cache_size > max_size_mb) {
+    rm(list = ls(cache), envir = cache)
+    gc()
+    return(TRUE)
+  }
+  FALSE
+}
+
+# Get period-specific universe_ppl_flag datasets ---------------------------
+period_specific_data <- reactive({
+  if(is.null(session$userData$period_cache)) {
+    session$userData$period_cache <- new.env()
+  }
+  cache <- session$userData$period_cache
+  
+  check_cache_size(cache)
+  
+  cache_key <- digest::digest(list(
+    if(isTruthy(input$in_demo_mode)) "demo" else input$imported$name,
+    
+    # Client-level filters
+    input$syso_age,
+    input$syso_race_ethnicity,
+    input$syso_spec_pops,
+    
+    # Enrollment-level filters
+    input$syso_hh_type,
+    input$syso_level_of_detail,
+    input$syso_project_type
+  ))
+  
+  cached_result <- cache[[cache_key]]
+  if (!is.null(cached_result)) {
+    return(cached_result)
+  }
+  
+  upload_name <- ifelse(input$in_demo_mode, "DEMO", input$imported$name)
+  
+  results <- lapply(
+    session$userData$report_dates,
+    function(period) {
+      all_filtered <- universe_filtered(period, upload_name)
+      universe_w_enrl_flags <- universe_enrl_flags(all_filtered, period)
+      universe_w_ppl_flags <- universe_ppl_flags(universe_w_enrl_flags, period)
+      
+      # Add month flag for month-periods
+      if(!identical(period, session$userData$report_dates[["Full"]])) {
+        universe_w_ppl_flags[, month := as.Date(period[1])]
+      }
+      universe_w_ppl_flags
+    }
+  )
+  cache[[cache_key]] <- results
+  session$userData$period_cache <- cache
+  results
+})
+
+# Client-level flags, filtered ----------------------------------------------------
+client_categories_filtered <- reactive({
+  req(nrow(session$userData$client_categories) > 0)
+  session$userData$client_categories[, All := 1][
+    AgeCategory %in% input$syso_age &
+      get(input$syso_race_ethnicity) == 1 &
+      (
+        input$syso_spec_pops == "None" |
+          (input$syso_spec_pops == "Veteran" &
+             VeteranStatus == 1 & !(AgeCategory %in% c("0 to 12", "13 to 17"))) |
+          (input$syso_spec_pops == "NonVeteran" &
+             VeteranStatus == 0 & !(AgeCategory %in% c("0 to 12", "13 to 17")))
+      )
+  ]
+})
+
+# Period-specific, user-filtered, enrollment-level universe applied ------------------
+universe_filtered <- function(period, upload_name) {
+  # browser()
+  join(
+    session$userData$get_period_specific_enrollment_categories(period, upload_name) %>%
+      fsubset(eecr | lecr | lookback),
+    session$userData$get_period_specific_nbn_enrollment_services(period, upload_name), 
+    on = "EnrollmentID"
+  ) %>%
+    join( # Inner Join with client categories
+      # This is necessary for bringing in Veteran Status, but will also make the rest faster
+      client_categories_filtered(),
+      # client_categories_filt, # this is used for mirai
+      on = "PersonalID",
+      how = "inner"
+    ) %>%
+    fsubset(
+      # Household type filter
+      (input$syso_hh_type == "All" |
+         (input$syso_hh_type == "YYA" & HouseholdType %in% c("PY", "UY")) |
+         (input$syso_hh_type == "YYA" & HouseholdType == "CO" & VeteranStatus != 1) | 
+         (input$syso_hh_type == "AO" & HouseholdType %in% c("AOminusUY","UY")) | 
+         (input$syso_hh_type == "AC" & HouseholdType %in% c("ACminusPY","PY")) | 
+         input$syso_hh_type == HouseholdType
+      ) &
+        # Level of detail filter
+        (input$syso_level_of_detail == "All" |
+           (input$syso_level_of_detail == "HoHsAndAdults" &
+              (MostRecentAgeAtEntry >= 18 | CorrectedHoH == 1)) |
+           (input$syso_level_of_detail == "HoHsOnly" &
+              CorrectedHoH == 1)) &
+        # Project type filter
+        (input$syso_project_type == "All" |
+           input$syso_project_type == eecr_project_type
+        )
+    )
+}
 
 update_sys_plot_data <- function() {
   sys_plot_data$inflow_outflow_full <- get_inflow_outflow_full()
