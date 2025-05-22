6cfd0f93b6476aa55386d60dc29d0253ce7367f4
Alex Silverman <silvermana@aneprrstu-01.ad.abt.local>
2025-05-22 10:48:20 -0400

    reset mirai daemons after shinytest

--- tests/testthat.R
+++ tests/testthat.R
@@ -4,3 +4,4 @@ test_app(".", filter="main-valid") # example filter: filter="main-valid"
 # testthat::snapshot_review()
 # testthat::snapshot_review(files="main-valid/test-main-valid-exportTestValues.json")
 # testthat::snapshot_accept(files="fsa/test-fsa-001.json")
+daemons(0)
