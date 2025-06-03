f849d926fbc003fda8b7212e52a2a8f7eb977cfb
Marschall Furman <51181259+mlfurman3@users.noreply.github.com>
2025-06-03 16:08:46 -0400

    fix missing reset_app session argument

--- demo_management.R
+++ demo_management.R
@@ -149,7 +149,7 @@ toggle_demo <- function(in_demo_mode) {
     removeModal()
     accordion_panel_open(id = 'accordion_home', values = 'home_demo_instructions')
   } else {
-    reset_app()
+    reset_app(session)
   }
   mode <- ifelse(in_demo_mode, 'demo', 'live')
   print(glue("Switched to {mode} mode!"))
