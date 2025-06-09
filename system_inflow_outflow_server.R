a8d81c1caf1c3c35bf728d445e7ae8ff9930a45b
Alex Silverman <silvermana@aneprrstu-01.ad.abt.local>
2025-06-09 13:42:56 -0400

    make sure to include months in x-axis label

--- system_inflow_outflow_server.R
+++ system_inflow_outflow_server.R
@@ -1248,7 +1248,8 @@ sys_monthly_single_status_ui_chart <- function(varname, status) {
     ) +
     scale_x_discrete(expand = expansion(mult = c(0.045, 0.045))) + # make plto take up more space horizontally
     theme(
-      axis.text = element_blank(),
+      axis.text.x = element_text(size = sys_axis_text_font),
+      axis.text.y = element_blank(),
       axis.title.y = element_text(size = sys_axis_text_font), 
       axis.title.x = element_blank(),
       legend.position = "none",
