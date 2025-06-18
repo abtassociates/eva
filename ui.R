0e2b5c59bd1b5c242d16a667ea724fcdb7f21c8a
Marschall Furman <51181259+mlfurman3@users.noreply.github.com>
2025-06-18 10:45:09 -0400

    add vertical line between Overview filter dropdowns - required reorganizing into two side-by-side cards

--- ui.R
+++ ui.R
@@ -609,66 +609,82 @@ nav_panel(
   card(
     card_header('Filters'),
     layout_columns(
-      col_widths = c(2,2,2,6, 3,3,6),
-      pickerInput(
-        label = "Household Type",
-        inputId = "syso_hh_type",
-        choices = syso_hh_types,
-        selected = syso_hh_types[1],
-        options = pickerOptions(container = "body")
-      ),
-      pickerInput(
-        label = "Level of Detail",
-        inputId = "syso_level_of_detail",
-        choices = syso_level_of_detail,
-        selected = syso_level_of_detail[1],
-        options = pickerOptions(container = "body")
-      ),
-      pickerInput(
-        label = "Project Type Group",
-        inputId = "syso_project_type",
-        choices = syso_project_types,
-        selected = syso_project_types[1],
-        options = pickerOptions(container = "body")
-      ),
-      pickerInput(
-        label = "Race/Ethnicity Methodology Type",
-        inputId = "methodology_type",
-        multiple = FALSE,
-        selected = syso_methodology_types[1],
-        choices = syso_methodology_types,
-        options = pickerOptions(container = "body")
-      ),
-      pickerInput(
-        inputId = "syso_age",
-        label = "Age",
-        selected = syso_age_cats,
-        choices = syso_age_cats,
-        multiple = TRUE,
-        options = pickerOptions(
-          actionsBox = TRUE,
-          selectedTextFormat = paste("count >", length(syso_age_cats)-1),
-          countSelectedText = "All Ages",
-          noneSelectedText = "All Ages",
-          container = "body"
+      col_widths=c(6,6),
+      gap = 0,
+      card(
+        id = 'card_filters1',
+        style='border-width:0;border-radius:0',
+        layout_columns(
+          col_widths = c(4,4,4,6,6),fill = T,
+          
+        pickerInput(
+          label = "Household Type",
+          inputId = "syso_hh_type",
+          choices = syso_hh_types,
+          selected = syso_hh_types[1],
+          options = pickerOptions(container = "body")
+        ),
+        pickerInput(
+          label = "Level of Detail",
+          inputId = "syso_level_of_detail",
+          choices = syso_level_of_detail,
+          selected = syso_level_of_detail[1],
+          options = pickerOptions(container = "body")
+        ),
+        pickerInput(
+          label = "Project Type Group",
+          inputId = "syso_project_type",
+          choices = syso_project_types,
+          selected = syso_project_types[1],
+          options = pickerOptions(container = "body")
+        ),
+        pickerInput(
+          inputId = "syso_age",
+          label = "Age",
+          selected = syso_age_cats,
+          choices = syso_age_cats,
+          multiple = TRUE,
+          options = pickerOptions(
+            actionsBox = TRUE,
+            selectedTextFormat = paste("count >", length(syso_age_cats)-1),
+            countSelectedText = "All Ages",
+            noneSelectedText = "All Ages",
+            container = "body"
+          )
+        ),
+        pickerInput(
+          label = "Veteran Status",
+          inputId = "syso_spec_pops",
+          choices = syso_spec_pops_people,
+          selected = syso_spec_pops_people[1],
+          options = pickerOptions(container = "body")
+        )
         )
       ),
-      pickerInput(
-        label = "Veteran Status",
-        inputId = "syso_spec_pops",
-        choices = syso_spec_pops_people,
-        selected = syso_spec_pops_people[1],
-        options = pickerOptions(container = "body")
-      ),
-      pickerInput(
-        label = "Race/Ethnicity",
-        inputId = "syso_race_ethnicity",
-        choices = syso_race_ethnicity_method1,
-        selected = syso_race_ethnicity_method1,
-        options = list(
-          `dropdown-align-right` = TRUE,
-          `dropup-auto` = FALSE,
-          container = "body"
+      card(
+        id = 'card_filters2',
+        style="border-width:0;border-left-width: 1px; border-radius:0",
+        layout_columns(
+          col_widths = c(12,12),
+        pickerInput(
+          label = "Race/Ethnicity Methodology Type",
+          inputId = "methodology_type",
+          multiple = FALSE,
+          selected = syso_methodology_types[1],
+          choices = syso_methodology_types,
+          options = pickerOptions(container = "body")
+        ),
+        pickerInput(
+          label = "Race/Ethnicity",
+          inputId = "syso_race_ethnicity",
+          choices = syso_race_ethnicity_method1,
+          selected = syso_race_ethnicity_method1,
+          options = list(
+            `dropdown-align-right` = TRUE,
+            `dropup-auto` = FALSE,
+            container = "body"
+          )
+        )
         )
       )
     )
