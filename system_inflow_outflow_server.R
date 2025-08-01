80fdbd19dbc337dd9a6154277401357e1029b562
Alex Silverman <silvermana@aneprrstu-01.ad.abt.local>
2025-08-01 05:51:38 -0400

    only "view" the bad records if in dev mode AND not in shinytest mode. This way the tests can proceed through these.

--- system_inflow_outflow_server.R
+++ system_inflow_outflow_server.R
@@ -352,7 +352,7 @@ universe_ppl_flags <- function(universe_df) {
     fsubset(InflowTypeDetail == "Unknown" & period == "Full")
   if(nrow(bad_records) > 0) {
     logToConsole(session, "ERROR: There's an Inflow-Unknown in the Full Annual data")
-    if(in_dev_mode) {
+    if(in_dev_mode & !isTRUE(getOption("shiny.testmode"))) {
       bad_records <- get_all_enrollments_for_debugging(bad_records, universe_w_ppl_flags) %>% 
         fselect(inflow_debug_cols)
       view(bad_records)
@@ -368,7 +368,7 @@ universe_ppl_flags <- function(universe_df) {
     )
   if(nrow(bad_records) > 0) {
     logToConsole(session, "ERROR: There are clients whose Inflow or Outflow is 'something's wrong'")
-    if(in_dev_mode) {
+    if(in_dev_mode & !isTRUE(getOption("shiny.testmode"))) {
       somethings_wrongs <- get_all_enrollments_for_debugging(bad_records, universe_w_ppl_flags, multiple=TRUE) %>%
         fgroup_by(PersonalID) %>%
         fmutate(
@@ -417,7 +417,7 @@ universe_ppl_flags <- function(universe_df) {
     )
   if(nrow(bad_records) > 0)  {
     logToConsole(session, "ERROR: There are clients whose first-month Inflow != Full Period Inflow and/or last-month Outflow != Full Period outflow")
-    if(in_dev_mode) {
+    if(in_dev_mode & !isTRUE(getOption("shiny.testmode"))) {
       bad_first_inflow_records <- get_all_enrollments_for_debugging(
         bad_records[first_enrl_month_inflow != full_period_inflow],
         universe_w_ppl_flags,
@@ -464,7 +464,7 @@ universe_ppl_flags <- function(universe_df) {
       (days_since_lookback > 14 | is.na(days_since_lookback))
     )
   if(nrow(bad_records) > 0) {
-    if(in_dev_mode) {
+    if(in_dev_mode & !isTRUE(getOption("shiny.testmode"))) {
       bad_ashomeless <- get_all_enrollments_for_debugging(
         bad_ashomeless,
         universe_w_ppl_flags,
