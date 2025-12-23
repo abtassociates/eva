f4420e8f440336b61927d1efcbb42c6261ea99e0
Sara Sokolinski <sokolinskis@aneprrstu-01.ad.abt.local>
2025-12-23 15:33:58 -0500

    Remove nightly average and ranged counting functions from 08_inv_util.R to server_09_inv_util.R - counting over the quarters takes a while, so moving it to the front end (server) to execute on selected projects only - remove these functions and the code using them - rename Bed_Utilization and Unit_Utilization to PIT_Bed_Utilization and PIT_Unit_Utilization, and multiply by 100, round to 1 decimal place and paste with a % - pass 'HMIS_project_active_inventories' and 'EnrollmentAdjust' to session$userData

--- 08_inv_util.R
+++ 08_inv_util.R
@@ -200,157 +200,17 @@ count_Enrollments <-function(pit_dates, extra_groups = NULL){
   return(Bed_Unit_Util)
 } 
 
-# create versions that sum counts over a range of dates
-# use difference in dates to calculate the 'report length' and calculate nightly averages
-count_Beds_Units_rng <- function(range_start,range_end, extra_groups = NULL){
-  if(length(extra_groups)==0){
-    grouping_vars <- c( "ProjectID")
-  }else{
-    grouping_vars <- c("ProjectID", extra_groups) %>% unique
-  }
-  
-  activeDays <- function(inv_start, inv_end){
-    overlap_start <- max(as.Date(inv_start), range_start, na.rm=TRUE)
-    overlap_end <- min(as.Date(inv_end), range_end, na.rm=TRUE)
-    if(overlap_start>overlap_end){
-      return(0)
-    }else{
-      return(as.numeric(overlap_end-overlap_start+1))
-    }
-  }
-  
-  Bed_Unit_Count <- HMIS_project_active_inventories %>% 
-    #fselect(ProjectID, BedInventory, UnitInventory, InventoryStartDate, InventoryEndDate) %>%
-    fmutate(
-      active_days = activeDays(InventoryStartDate, InventoryEndDate)
-    ) %>%
-    fgroup_by(grouping_vars) %>%
-    fsummarize(
-      Total_Beds = fsum(active_days*BedInventory),
-      Total_Units = fsum(active_days*UnitInventory)
-    ) %>% fungroup()
-  
-  # calculate length of range 
-  report_length <- as.numeric(range_end - range_start)
-  Bed_Unit_Count <- Bed_Unit_Count %>% # use it to calculate average nightly beds/units
-    fmutate(Avg_Nightly_Beds = Total_Beds / report_length ,
-            Avg_Nightly_Units = Total_Units / report_length )
-  
-  return(Bed_Unit_Count)
-}
-count_Enrollments_rng <-function(range_start,range_end, extra_groups = NULL){
-  if(length(extra_groups)==0){
-    grouping_vars <- c( "ProjectID", "ProjectType")
-  }else{
-    grouping_vars <- c("ProjectID", "ProjectType", extra_groups) %>% unique
-  }
-  browser()
-  activeDays <- function(inv_start, inv_end){
-    overlap_start <- max(as.Date(inv_start), range_start, na.rm=TRUE)
-    overlap_end <- min(as.Date(inv_end), range_end, na.rm=TRUE)
-    if(overlap_start>overlap_end){
-      return(0)
-    }else{
-      return(as.numeric(overlap_end-overlap_start+1))
-    }
-  }
-  #For each relevant project and using the EnrollmentAdjust data frame, count the number of people "served in a bed" on each of the 4 PIT Dates.
-  #For an enrollment to be considered "active" on a PIT Date it must meet the following logic: EntryDate <= [PIT Date] and ExitAdjust > [PIT Date] or is NULL
-  #Exclude any ES - NbN enrollments where there is no Bed Night record on [PIT Date]
-  #Exclude any permanent housing enrollments where MoveInDateAdjust < [PIT Date]
-  services_qPIT <- Services %>%
-    fselect(EnrollmentID, DateProvided)  %>% 
-    fmutate(bn_rng = fifelse(as.Date(DateProvided) > range_start & as.Date(DateProvided) <= range_end, 1, 0)) # 1 or 0 day in range
-  
-  Bed_Unit_Util <- EnrollmentAdjust %>%
-    join(services_qPIT, on = "EnrollmentID", how = "left") %>%
-    fsubset(!(ProjectType %in% c(3,9,10,13)) | !is.na(MoveInDateAdjust)) %>% # drop perm housing project enrollments if move in date missing
-    fsubset(!(ProjectType %in% c(3,9,10,13)) | MoveInDateAdjust >= DateProvided) # drop perm housing project enrollments if move in date after DateProvided
-  
-  Bed_Unit_Util$active_days <- mapply(activeDays, inv_start = Bed_Unit_Util$EntryDate, inv_end = Bed_Unit_Util$ExitAdjust) # count days of enrollment overlapping with days of range
-  
-  Bed_Unit_Util <- Bed_Unit_Util %>% fmutate(# Enrollment Active
-      enrollDays = fifelse(!is.na(bn_rng), bn_rng, active_days)) %>% # if row is in services, use bn_rng to count days, otherwise, use active_days
-    fmutate(HHServed = fifelse(RelationshipToHoH==1, # count households by just flagging active/elig enrollments that are head of household
-                               enrollDays, 0)) %>%
-    fgroup_by(grouping_vars) %>% 
-    fsummarise(
-      eligProjNBN = any(enrollDays>0),
-      Total_Served = fsum(enrollDays),
-      Total_HHServed = fsum(HHServed))%>%
-    fungroup()  %>% fsubset(!is.na(Total_Served) & (ProjectType != es_nbn_project_type |eligProjNBN))
-  
-  # calculate length of range 
-  report_length <- as.numeric(range_end - range_start )
-  Bed_Unit_Util <- Bed_Unit_Util %>% # use it to calculate average served beds/units
-    fmutate(Avg_Nightly_Served = Total_Served / report_length ,
-            Avg_Nightly_HHServed = Total_HHServed / report_length ) %>%
-    select(-eligProjNBN, -ProjectType)
-  
-  return(Bed_Unit_Util)
-} 
-
-nightly_avg <- function(period, labels ){
-  
-  for (q in 1:length(period)){
-    
-    if(q!=length(period)){ # IF NOT LAST
-      nightly_avg_q <- count_Beds_Units_rng(period[q], period[q+1]) %>%
-        join(count_Enrollments_rng(period[q], period[q+1]), how = "left") %>%
-        fmutate(PIT = period[q],
-                label = labels[q])
-      
-    }else{ # IF LAST, use a year from first quarter minus a day (so full range is 365)
-      nightly_avg_q <- count_Beds_Units_rng(period[q], period[1] + years(1) - days(1)) %>%
-        join(count_Enrollments_rng(period[q], period[1] + years(1) - days(1)), how = "left") %>%
-        fmutate(PIT = period[q],
-                label = labels[q])
-    }
-    if(q==1){
-      nightly_avg <- nightly_avg_q
-    }else{
-      nightly_avg <- nightly_avg %>% rowbind(nightly_avg_q)
-    }
-    rm(nightly_avg_q) # delete quarter
-  }
-  
-  nightly_avg_ann <- nightly_avg %>% fgroup_by(ProjectID) %>%
-    fsummarise(Total_Beds = fsum(Total_Beds),
-               Total_Units = fsum(Total_Units),
-               Total_Served = fsum(Total_Served),
-               Total_HHServed = fsum(Total_HHServed)) %>% 
-    fungroup %>%
-    fmutate(
-      PIT = as.Date(NA),
-      label = "Annual",
-      Avg_Nightly_Beds = Total_Beds / 365,
-      Avg_Nightly_Units = Total_Units / 365,
-      Avg_Nightly_Served = Total_Served / 365,
-      Avg_Nightly_HHServed = Total_HHServed / 365
-    )
-  
-  nightly_avg <- nightly_avg %>% rowbind(nightly_avg_ann) %>%
-    fmutate(Avg_Nightly_Bed_Util = Avg_Nightly_Served / Avg_Nightly_Beds,
-            Avg_Nightly_Unit_Util = Avg_Nightly_HHServed / Avg_Nightly_Units)
-  return(nightly_avg)
-}
 quarters <- get_quarters() %>% sort
 
 # full join the results of passing quarters through counting functions
 # Counts on PIT Dates
 project_level_util_q <- count_Beds_Units(quarters) %>%
-  join(count_Enrollments(quarters), how = "left")
-
-# Avg over Quarters 
-nightly_avg <- nightly_avg(period = quarters, labels = names(quarters))
-
-project_level_util_q <- project_level_util_q %>% join(nightly_avg, how = "full") 
-rm(nightly_avg)
-
-# calculate project level quarterly utilization
-project_level_util_q <- project_level_util_q %>%
-  fmutate(Bed_Utilization = PIT_Served / PIT_Beds,
-          Unit_Utilization = PIT_HHServed / PIT_Units)
+  join(count_Enrollments(quarters), how = "left") %>%
+  fmutate(PIT_Bed_Utilization = paste(round(PIT_Served / PIT_Beds *100, digits = 1), "%"),
+          PIT_Unit_Utilization = paste(round(PIT_HHServed / PIT_Units*100, digits = 1), "%"))
 
 # pass results to session for server_09_inv_util.R
 session$userData$project_level_util_q <- project_level_util_q 
+session$userData$HMIS_project_active_inventories <- HMIS_project_active_inventories 
+session$userData$EnrollmentAdjust <- EnrollmentAdjust
+
