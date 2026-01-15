17b84c4188c61c24d410650976bb1b732aed4621
Marschall Furman <51181259+mlfurman3@users.noreply.github.com>
2026-01-15 11:28:14 -0500

    add extra logging statement

--- client_level_export_server.R
+++ client_level_export_server.R
@@ -229,9 +229,12 @@ output$client_level_download_btn <- downloadHandler(
       "Monthly Statuses",
       "Adjusted Non-Res"
     )
+    
     logToConsole(session, paste0("after naming, length(client_level_export_list) = ", length(client_level_export_list)))
     logToConsole(session, paste0(
       'class: ', paste0(names(client_level_export_list), ': ', sapply(client_level_export_list, class), collapse=', ')))
+    tmp <- unlist(lapply(lapply(client_level_export_list, nchar),function(x) which(x > 32000)))
+    logToConsole(session,  paste0("max fields: ", paste0(names(tmp), ': ', tmp, collapse=', ')))
     logToConsole(session, paste0(
       'max characters: ', paste0(names(client_level_export_list), ': ', unlist(lapply(lapply(client_level_export_list, nchar), max)), collapse=', ')))
     logToConsole(session, paste0("dimensions: ", paste0(names(client_level_export_list),': ',sapply(client_level_export_list, nrow, simplify=T), 'x',sapply(client_level_export_list, ncol, simplify=T), collapse=', ')))
