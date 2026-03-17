6d75eb0f3767627bf759253c2b0e22f3c6111d1f
Alex Silverman <silvermana@aneprrstu-01.ad.abt.local>
2026-03-16 22:19:17 -0400

    For DQ Null Unless, there's no CSV or Name field, so need to add the Name field to the detail

--- load_machine_readable_specs.R
+++ load_machine_readable_specs.R
@@ -106,7 +106,14 @@ reporting_info <- reporting_info |>
   join(
     readxl::read_xlsx(validation_specs_bk, sheet = "Detail Texts"),
     on = c("issue_type" = "Issue Type")
-  ) 
+  ) |>
+  fmutate(
+    `Detail Text` = fifelse(
+      Issue == "Null Unless",
+      gsub("This field", "{Name}", `Detail Text`),
+      `Detail Text`
+    )
+  )
   
 # Read in CSV Lists from Specs ---------------------
 # This tab contains all the valid values for each list element
