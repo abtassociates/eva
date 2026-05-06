4fc54ab0537e3ca65316125a3688c603b2136dc3
Marschall Furman <51181259+mlfurman3@users.noreply.github.com>
2026-05-06 16:59:06 -0400

    correction to prior commit - convert system comp 2D code to collapse

--- system_composition_server.R
+++ system_composition_server.R
@@ -194,13 +194,14 @@ sys_comp_plot_2vars <- function(subtab = 'comp', methodology_type, selections, i
     replace(is.na(.), 0)
   
   if(methodology_type == 1) {
-    h_total <- plot_df %>%
-      group_by(!!!syms(selections[[2]])) %>%
-      summarise(N = ifelse(all(is.na(n)), NA, sum(n, na.rm = TRUE))) %>%
-h_total2 <- plot_df %>%
-  fgroup_by(selections[2]) %>%
-  fsummarise(N = fsum(n))
-h_total2[[selections[1]]] <- "Total"
+   
+    h_total_tmp <- plot_df %>%
+      fgroup_by(selections[2]) %>%
+      fsummarise(N = fsum(n))
+   
+    h_total_tmp[[selections[1]]] <- "Total"
+    
+    h_total <- h_total_tmp %>% 
       suppress_values("N") %>%
       suppress_next_val_if_one_suppressed_in_group(selections[1], "N")
     
