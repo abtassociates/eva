84bc9e5bbb707898c2d345e0bb90609c53f9dbb7
Marschall Furman <51181259+mlfurman3@users.noreply.github.com>
2026-06-02 16:27:29 -0400

    fix logging dependencies to avoid glue pkg

--- 06_PDDE_Checker.R
+++ 06_PDDE_Checker.R
@@ -271,9 +271,9 @@ vsps_in_hmis <- session$userData$Project0 %>%
 # Zero Utilization --------------------------------------------------------
 # HMIS participating projects that have ANY active inventory (with available beds) 
 # should not have 0 enrollments
-logToConsole(session, glue('dim(ProjectSegments): {fnrow(ProjectSegments)}x{fncol(ProjectSegments)}'))
-logToConsole(session, glue('num NA HMISPart.: {sum(is.na(ProjectSegments$HMISParticipationType))}'))
-logToConsole(session, glue('num 0 HMISPart.: {sum(ProjectSegments$HMISParticipationType == 0, na.rm=T)}'))
+logToConsole(session, paste0('dim(ProjectSegments): ', fnrow(ProjectSegments), 'x',fncol(ProjectSegments)))
+logToConsole(session, paste0('num NA HMISPart.: ',sum(is.na(ProjectSegments$HMISParticipationType))))
+logToConsole(session, paste0('num 0 HMISPart.: ',sum(ProjectSegments$HMISParticipationType == 0, na.rm=T)))
 
 zero_utilization <- qDT(ProjectSegments) %>%
   # HMiS-participating projects
