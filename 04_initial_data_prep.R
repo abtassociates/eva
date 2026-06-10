901b27a25b981dcd8facc6fb18e6a100155acf9b
Sara Sokolinski <sokolinskis@aneprrstu-01.ad.abt.local>
2026-06-10 12:10:52 -0400

    04_initial_data_prep.R
    # Update EnrollmentStaging
    - calculate column AgeAtReportStart by passing DOB and the max of meta_HUDCSV_Export_Start and ReportStart into function age_years()
    - calculate max_AgeAtReportStart and min_AgeAtReportStart as the fmax/fmin of AgeAtReportStart when grouping by ProjectID and HouseholdID
    - translate nested fifelse into fcase when calculating HouseholdType
    - calculate 'HHTypeAtRecordStart' like 'HouseholdType' but substituting AgeAtReportStart, min_AgeAtReportStart & max_AgeAtReportStart for AgeAtEntry, min_AgeAtEntry & max_AgeAtEntry

--- 04_initial_data_prep.R
+++ 04_initial_data_prep.R
@@ -85,44 +85,46 @@ rm(project_prep)
 
 # Enrollment --------------------------------------------------------------
 # Truncating Enrollments based on Operating/Participating -----------------
+
 EnrollmentStaging <- Enrollment %>%
   join(Client %>% fselect(PersonalID, DOB), 
        on = "PersonalID") %>%
   join(Exit %>% fselect(EnrollmentID, Destination, DestinationSubsidyType, ExitDate),
        on = "EnrollmentID") %>%
   fmutate(ExitAdjust = fcoalesce(ExitDate, no_end_date),
-         AgeAtEntry = age_years(DOB, EntryDate),
-         DOB = NULL) %>%
+          AgeAtEntry = age_years(DOB, EntryDate),
+          AgeAtReportStart = age_years(DOB, max(session$userData$meta_HUDCSV_Export_Start, session$userData$ReportStart, na.rm=TRUE)),
+          DOB = NULL) %>%
   fgroup_by(ProjectID, HouseholdID) %>%
   fmutate(
     max_AgeAtEntry = fmax(AgeAtEntry),
     min_AgeAtEntry = fmin(AgeAtEntry),
+    max_AgeAtReportStart = fmax(AgeAtReportStart),
+    min_AgeAtReportStart = fmin(AgeAtReportStart),
     # DomesticViolenceCategory = fcase(
     #   DomesticViolenceSurvivor == 1 & CurrentlyFleeing == 1, "DVFleeing",
     #   DomesticViolenceSurvivor == 1, "DVNotFleeing",
     #   default = "NotDV"
     # ),
     HouseholdType = factor(
-      fifelse(
-        any(between(AgeAtEntry, 0, 17)) & max_AgeAtEntry >= 18,
-        fifelse(
-          between(max_AgeAtEntry, 0, 24),
-          "PY",
-          "ACminusPY"
-        ),
-        fifelse(
-          min_AgeAtEntry >= 18,
-          fifelse(
-            between(max_AgeAtEntry, 0, 24),
-            "UY", # UY = Unaccompanied Youth. YYA = PY + UY + CO
-            "AOminusUY"
-          ),
-          fifelse(
-            min_AgeAtEntry >= 0 & max_AgeAtEntry <= 17,
-            "CO", 
-            "UN"
-          )
-        )
+      
+      fcase(any(between(AgeAtEntry, 0, 17)) & between(max_AgeAtEntry, 18, 24), "PY",
+            any(between(AgeAtEntry, 0, 17)) & max_AgeAtEntry >= 18, "ACminusPY",
+            min_AgeAtEntry >= 18 & between(max_AgeAtEntry, 0, 24), "UY", 
+            min_AgeAtEntry >= 18, "AOminusUY",
+            min_AgeAtEntry >= 0 & max_AgeAtEntry <= 17, "CO", 
+            default = "UN"
+      ),
+      levels = c("AOminusUY", "ACminusPY", "CO", "UN", "PY", "UY")
+    ),
+    HHTypeAtRecordStart = factor(
+      
+      fcase(any(between(AgeAtReportStart, 0, 17)) & between(max_AgeAtReportStart, 18, 24), "PY",
+            any(between(AgeAtReportStart, 0, 17)) & max_AgeAtReportStart >= 18, "ACminusPY",
+            min_AgeAtReportStart >= 18 & between(max_AgeAtReportStart, 0, 24), "UY", 
+            min_AgeAtReportStart >= 18, "AOminusUY",
+            min_AgeAtReportStart >= 0 & max_AgeAtReportStart <= 17, "CO", 
+            default = "UN"
       ),
       levels = c("AOminusUY", "ACminusPY", "CO", "UN", "PY", "UY")
     )
