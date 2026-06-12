efccd0a7279057b9e7c9e66c896ef8e89f2afa73
Marschall Furman <51181259+mlfurman3@users.noreply.github.com>
2026-06-12 10:52:14 -0400

    add fselect to FSA no_client and no_enrollment checks so that rbindlist works properly

--- changelog.R
+++ changelog.R
@@ -8,6 +8,7 @@ output$changelog <- renderDT({
       - System Exits charts, visualizations, and exports <br>
     <b>Bug Fixes</b> <br>
       - Fixed incorrect zero utilization warning for projects with multiple HMIS Participation Records (Issue <a href='https://github.com/abtassociates/eva/issues/994' target='_blank'>#994</a>)<br>
+      - Added FSA check for when Client.csv has no records <br>
     <b>Miscellaneous</b> <br>
       - Renamed Glossary page to System Performance Glossary <br>
       - Updated System Performance Glossary to include new entries related to System Exits
