efe7f1f02384c012d81a93fef73489d10de96951
Marschall Furman <51181259+mlfurman3@users.noreply.github.com>
2026-06-29 16:27:00 -0400

    drop CLS column in get_active_info, which should only be there for unsheltered tab. not needed at this point in the process

--- system_overview_server.R
+++ system_overview_server.R
@@ -313,7 +313,7 @@ enrollments_filtered <- reactive({
 get_active_info <- function(all_filtered_by_period, all_filtered, lh_info_df = session$userData$lh_info,
                             reportStart = session$userData$ReportStart, reportEnd = session$userData$ReportEnd) {
   lh_info_filtered <- lh_info_df %>%
-    fselect(-first_lh_date, -last_lh_date, -lh_prior_livingsituation) %>%
+    fselect(-first_lh_date, -last_lh_date, -lh_prior_livingsituation, -CurrentLivingSituation) %>%
     join(
       all_filtered %>% fselect(EnrollmentID, EntryDate, ExitAdjust),
       on = "EnrollmentID",
