8fc19ecce147a6afea80bcb1b2266a786f3407e7
Alex Silverman <silvermana@aneprrstu-01.ad.abt.local>
2026-07-09 16:19:26 -0400

    fix calculations: - using latest enrollment (by EntryDate for all metrics except for Successful Exits and IncomeGrowth, where we use ExitDate) - fix naming of Entered into Non-Habitat metric - other minor typos - fix income growth calculation - add render functions for easier formatting - update UI for more consistent formatting and better handling of nulls/empties.

--- project_dashboard_metrics_server.R
+++ project_dashboard_metrics_server.R
@@ -1,105 +1,134 @@
 metrics <- reactive({
   enrollment_w_project_type <- session$userData$Enrollment |> 
-    fsubset(EntryDate >= input$dateRangeCount[1] & ExitAdjust <= input$dateRangeCount[2]) |>
+    fsubset(EntryDate %between% input$dateRangeCount | ExitAdjust %between% input$dateRangeCount) |>
     join(session$userData$Project0 |> fselect(ProjectID, ProjectType), on = "ProjectID") 
   
-  los_dt <- enrollment_w_project_type |>
+  latest_entry <- enrollment_w_project_type[EntryDate %between% input$dateRangeCount]
+  latest_entry <- if(fnrow(latest_entry) > 0)
+    latest_entry |>
+      fgroup_by(PersonalID, EntryDate) |>
+      fslice(how = "last")
+  else
+    data.table()
+    
+  latest_exit <- enrollment_w_project_type[ExitDate %between% input$dateRangeCount]
+  latest_exit <- if(fnrow(latest_exit) > 0)
+    latest_exit |>
+      fsubset(ExitDate %between% input$dateRangeCount) |>
+      fgroup_by(PersonalID, ExitDate) |>
+      fslice(how = "last")
+  else
+    data.table()
+  
+  los_dt <- latest_entry |>
     fsubset(ProjectType %in% c(lh_residential_project_types, ph_project_types))
   
-  entered_non_habitat_dt <- enrollment_w_project_type |>
+  entered_non_habitat_dt <- latest_entry |>
     fsubset(
       ProjectType %in% c(lh_residential_project_types, setdiff(non_res_project_types, hp_project_type))	&
-        (RelationshipToHoH == 1 | AgeAtEntry >= 18), 
+      (RelationshipToHoH == 1 | AgeAtEntry >= 18), 
       EnrollmentID, LivingSituation
     )
   
   zero_income_dt <- session$userData$IncomeBenefits |>
     fsubset(DataCollectionStage == 1, EnrollmentID, IncomeFromAnySource) |>
     join(
-      enrollment_w_project_type |> fsubset(ProjectType == hp_project_type & (RelationshipToHoH == 1 | AgeAtEntry >= 18), EnrollmentID),
+      latest_entry |> fsubset(ProjectType == hp_project_type & (RelationshipToHoH == 1 | AgeAtEntry >= 18), EnrollmentID),
       on = "EnrollmentID",
       how = "inner"
     )
+    
   
-  success_ful_exit_dt <- session$userData$Exit |>
-    fsubset(!is.na(ExitDate)) |>
+  successful_exit_dt <- session$userData$Exit |>
     join(
-      enrollment_w_project_type |> fselect(EnrollmentID, ProjectType),
+      latest_exit |> fselect(EnrollmentID, ProjectType),
       on = "EnrollmentID",
       how = "inner"
     ) |>
     fmutate(
       successful_exit = fcase(
         ProjectType == out_project_type, Destination %in% setdiff(c(100:499),c(116,206,207,329)),
-        ProjectType %in% c(es_ee_project_type, es_nbn_project_type, th_project_type) = c(332,400:499),
-        default = c(400:499)
+        ProjectType %in% c(es_ee_project_type, es_nbn_project_type, th_project_type), Destination %in% c(332,400:499),
+        default = Destination %in% c(400:499)
       )
     ) |>
     fsubset(
       !(
         (Destination %in% c(24, 206) & ProjectType %in% lh_ph_hp_project_types) |
-        (Destination %in% c(215, 225) & ProjectType %in% setdiff(lh_ph_hp_project_types, out_project_type)) |
-        (Destination == 329 & ProjectType == out_project_type)
+          (Destination %in% c(215, 225) & ProjectType %in% setdiff(lh_ph_hp_project_types, out_project_type)) |
+          (Destination == 329 & ProjectType == out_project_type)
       )
     )
   
-  income_growth_dt <- session$userData$IncomeBenefits |>
-    join(
-      enrollment_w_project_type |> fsubset(ProjectType %in% c(ph_project_types, hp_project_type), EnrollmentID, EntryDate, ExitAdjust),
-      on = "EnrollmentID",
-      how = "inner"
-    ) |>
-    fmutate(TotalIncome = TotalMonthlyIncome*12) |>
-    # fselect(EnrollmentID, DataCollectionStage, TotalIncome, InformationDate, EntryDate, ExitAdjust) |>
-    fmutate(
-      income_at_entry = fifelse(DataCollectionStage == 1, TotalIncome, NA),
-      income_at_exit = fifelse(DataCollectionStage == 3, TotalIncome, NA)
-    ) |>
-    roworder(EnrollmentID, EntryDate) |>
-    fgroup_by(EnrollmentID) |>
-    fmutate(income_at_entry = ffirst(income_at_entry)) |>
-    fungroup() |>
-    roworder(EnrollmentID, ExitAdjust) |>
-    fgroup_by(EnrollmentID) |>
-    fmutate(income_at_exit = flast(income_at_entry)) |>
-    fslice(how = "first") |>
-    fungroup()
+  income_growth_latest_enrl <- latest_exit[ProjectType %in% c(ph_project_types, hp_project_type)]
+  income_growth_dt <- if(fnrow(session$userData$IncomeBenefits) > 0 && fnrow(income_growth_latest_enrl) > 0)
+    session$userData$IncomeBenefits |>
+      join(
+        income_growth_latest_enrl |> fselect(EnrollmentID),
+        on = "EnrollmentID",
+        how = "inner"
+      ) |>
+      fmutate(
+        income_at_entry = fifelse(DataCollectionStage == 1, TotalMonthlyIncome, NA),
+        income_at_exit = fifelse(DataCollectionStage == 3, TotalMonthlyIncome, NA)
+      ) |>
+      fgroup_by(EnrollmentID) |>
+      fmutate(
+        income_at_entry = ffirst(income_at_entry),
+        income_at_exit = flast(income_at_exit)
+      ) |>
+      fslice(how = "first") |>
+      fungroup() |>
+      fmutate(has_income_growth = income_at_exit > income_at_entry)
+    else
+      data.table()
   
   list(
-    avg_los = mean(los_dt$LengthOfStay, na.rm=TRUE),
-    median_los = median(los_dt$LengthOfStay, na.rm=TRUE),
-    entered_non_habitat_pct = fnrow(entered_non_habitat_dt[LivingSituation == 116L])/fnrow(entered_non_habitat_dt),
-    successful_exit_pct = fnrow(success_ful_exit_dt[successful_exit == TRUE])/fnrow(success_ful_exit_dt),
+    los_avg = mean(los_dt$LengthOfStay, na.rm=TRUE),
+    los_median = median(los_dt$LengthOfStay, na.rm=TRUE),
+    los_nmiss = fsum(is.na(los_dt$LengthOfStay)),
+    
+    entered_non_habitat_pct = fsum(entered_non_habitat_dt$LivingSituation == 116L)/fnrow(entered_non_habitat_dt[!is.na(LivingSituation)]),
+    entered_non_habitat_nmiss = fsum(is.na(enrollment_w_project_type$LivingSituation)),
+    
+    successful_exit_pct = fsum(successful_exit_dt$successful_exit)/fnrow(successful_exit_dt),
+    successful_exit_nmiss = fsum(is.na(successful_exit_dt$Destination)),
+    
     zero_income_pct = fnrow(zero_income_dt[is.na(IncomeFromAnySource) | IncomeFromAnySource == 0])/fnrow(zero_income_dt),
-    income_growth_pct = fnrow(income_growth_dt[income_growth > 0])/fnrow(income_growth_dt),
+    zero_income_nmiss = fsum(is.na(zero_income_dt$IncomeFromAnySource)),
+    
+    income_growth_pct = if(fnrow(income_growth_dt) > 0) fsum(income_growth_dt$has_income_growth)/fnrow(income_growth_dt[!is.na(TotalMonthlyIncome)]) else NA,
+    income_growth_nmiss = fsum(is.na(income_growth_dt$TotalMonthlyIncome)),
+    
     ce_assessments = session$userData$CEAssessedHouseholds
   )  
 })
 
-output$los_avg <- renderText({
-  sprintf("%.1f days", metrics()$avg_los)
-})
+render_pct <- function(val) {
+  if(length(val) > 0)
+    return(percent(val))
+  else
+    return("-")
+}
 
-output$los_median <- renderText({
-  sprintf("%d days", metrics()$median_los)
-})
+render_nmiss <- function(val) {
+  sprintf("%d enrollments", scales::comma(val))
+}
 
-output$entered_homeless_pct <- renderText({
-  sprintf("%.0f%%", metrics()$entered_homeless_pct)
-})
+output$los_avg <- renderText({sprintf("%.1f days", metrics()$los_avg)})
+output$los_median <- renderText({sprintf("%d days", metrics()$los_median)})
+output$los_nmiss <- renderText({render_nmiss(metrics()$los_nmiss)})
 
-output$successful_exit_pct <- renderText({
-  sprintf("%.0f%%", metrics()$successful_exit_pct)
-})
+output$entered_non_habitat_pct <- renderText({render_pct(metrics()$entered_non_habitat_pct)})
+output$entered_non_habitat_nmiss <- renderText({render_nmiss(metrics()$entered_non_habitat_nmiss)})
 
-output$zero_income_pct <- renderText({
-  sprintf("%.0f%%", metrics()$zero_income_pct)
-})
+output$successful_exit_pct <- renderText({render_pct(metrics()$successful_exit_pct)})
+output$successful_exit_nmiss <- renderText({render_nmiss(metrics()$successful_exit_nmiss)})
 
-output$income_growth_pct <- renderText({
-  sprintf("%.0f%%", metrics()$income_growth_pct)
-})
+output$zero_income_pct <- renderText({render_pct(metrics()$zero_income_pct)})
+output$zero_income_nmiss <- renderText({render_nmiss(metrics()$zero_income_nmiss)})
 
-output$ce_assessments <- renderText({
-  scales::comma(metrics()$ce_assessments)
-})
+output$income_growth_pct <- renderText({render_pct(metrics()$income_growth_pct)})
+output$income_growth_nmiss <- renderText({render_nmiss(metrics()$income_growth_nmiss)})
+
+output$ce_assessments <- renderText({scales::comma(metrics()$ce_assessments)})
--- ui.R
+++ ui.R
@@ -428,57 +428,60 @@ page_navbar(
                 gap = "1rem",
                 
                 value_box(
-                  title = "Length of Stay (All Households)",
+                  class = "project_dashboard_valbox",
+                  title = "Length of Stay in Residence (All Households)",
                   value = tagList(
                     div("Average: ", textOutput("los_avg", inline = TRUE)),
-                    div("Median: ", textOutput("los_median", inline = TRUE))
+                    div("Median: ", textOutput("los_median", inline = TRUE)),
+                    div("Missing LOS: ", textOutput("los_nmiss", inline = TRUE))
                   ),
                   showcase = bs_icon("house-door")
                 ),
                 
                 value_box(
+                  class = "project_dashboard_valbox",
                   title = "Entered from Place Not Meant for Habitation (HoHs/Adults)",
-                  value = div(
-                    class = "d-inline",
-                    textOutput("entered_non_habitat_pct", inline = TRUE),
-                    " of all HoHs/Adults"
+                  value = tagList(
+                    div("Percent of all HoHs/Adults: ", textOutput("entered_non_habitat_pct", inline = TRUE)),
+                    div("Missing LivingSituation: ", textOutput("entered_non_habitat_nmiss", inline = TRUE))
                   ),
                   showcase = bs_icon("signpost-split")
                 ),
                 
                 value_box(
+                  class = "project_dashboard_valbox",
                   title = "Successful Exits (All Clients)",
-                  value = div(
-                    class = "d-inline",
-                    textOutput("successful_exit_pct", inline = TRUE),
-                    " of all exited clients"
+                  value = tagList(
+                    div("Percent of all exited clients: ", textOutput("successful_exit_pct", inline = TRUE)),
+                    div("Missing Destination: ", textOutput("successful_exit_nmiss", inline = TRUE))
                   ),
                   showcase = bs_icon("check-circle")
                 ),
                 
                 value_box(
+                  class = "project_dashboard_valbox",
                   title = "Zero Income at Entry (HoHs/Adults)",
-                  value = div(
-                    class = "d-inline",
-                    textOutput("zero_income_pct", inline = TRUE),
-                    " of all HoHs/Adults"
+                  value = tagList(
+                    div("Percent of all HoHs/Adults: ", textOutput("zero_income_pct", inline = TRUE)),
+                    div("Missing IncomeFromAnySource: ", textOutput("zero_income_nmiss", inline = TRUE))
                   ),
                   showcase = bs_icon("wallet2")
                 ),
                 
                 value_box(
+                  class = "project_dashboard_valbox",
                   title = "Income Growth from Entry to Exit (HoHs/Adults)",
-                  value = div(
-                    class = "d-inline",
-                    textOutput("income_growth_pct", inline = TRUE),
-                    " of all exited HoHs/Adults"
+                  value = tagList(
+                    div("Percent of all exited HoHs/Adults: ", textOutput("income_growth_pct", inline = TRUE)),
+                    div("Missing TotalMonthlyIncome: ", textOutput("income_growth_nmiss", inline = TRUE))
                   ),
                   showcase = bs_icon("graph-up-arrow")
                 ),
                 
                 value_box(
+                  class = "project_dashboard_valbox",
                   title = "CE Assessments (HoHs Only)",
-                  value = textOutput("ce_assessments", inline = TRUE),
+                  value = div("Number of CE Assessments: ", textOutput("ce_assessments", inline = TRUE)),
                   showcase = bs_icon("clipboard-check")
                 )
               )
