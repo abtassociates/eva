2e8536875ca7292f145476599d50b83ef3508eb7
Sara Sokolinski <sokolinskis@aneprrstu-01.ad.abt.local>
2026-08-12 14:55:00 -0400

    fix merge conflicts in 'server.R' (after merging from dev)
    - remove sourcing 'project_dashboard_server.R'
    - delete 'project_dashboard_server.R' which was properly merged into 'rcode/server_06_project_dashboard.R' in the last commit

--- server.R
+++ server.R
@@ -65,17 +65,21 @@ function(input, output, session) {
   # this _supp renderUI needed to be associated with an output in order to make 
   # the HTML <div> id the same each time. Without associating with an output, 
   # the id changed each time and the shinytest would catch the difference and fail
-  output$headerClientCounts_supp <- renderUI({ 
+  output$headerProjectDashboard_supp <- renderUI({ 
     req(session$userData$valid_file() == 1)
     organization <- session$userData$Project0 %>%
-      fsubset(ProjectName == input$currentProviderList) %>%
+      fsubset(ProjectID == input$currentProviderList) %>%
       pull(OrganizationName)
     
-    h4(organization, "|", input$currentProviderList)
+    project_name <- session$userData$Project0 %>%
+      fsubset(ProjectID == input$currentProviderList) %>%
+      pull(ProjectName)
+    
+    h4(organization, "|", project_name)
   })
   
-  output$headerClientCounts <- headerGeneric(session, "Project Dashboard Report",
-                                             htmlOutput("headerClientCounts_supp"))
+  output$headerProjectDashboard <- headerGeneric(session, "Project Dashboard Report",
+                                             htmlOutput("headerProjectDashboard_supp"))
   
   output$headerPDDE <- headerGeneric(session, "Project Descriptor Data Elements Checker")
   
