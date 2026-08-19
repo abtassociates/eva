f1ec2e5c77bb805c321d6ae8a54f49104d50d767
Alex Silverman <silvermana@aneprrstu-01.ad.abt.local>
2026-08-19 11:41:20 -0400

    fixing bugs with System Exits by PH Demographics. While much of the code for this is similar to the System (Overview) Demographics code, there are key differences, mainly stemming from the fact that System Exits joins with PH demographics to form the numerator, vs. having the same denominator.
    - slightly different margin totals compared to System (Overview) Demographics
    - need a specific `syse_phd_plot` to handle differences instead of the `sys(o)_comp_plot`.

--- rcode/server_07.2.4_syse_phd.R
+++ rcode/server_07.2.4_syse_phd.R
@@ -27,23 +27,65 @@ sys_phd_selections_summary <- function() {
   )
 }
 
-sys_phd_plot <- function(subtab = 'phd', methodology_type, selections, isExport = FALSE) {
+# Helper to construct the PhD Excel export data frame (handles both 1D and 2D)
+create_phd_export_df <- function(plot_df_joined, plot_df_supp, selections) {
+  export_df <- plot_df_joined %>%
+    join(
+      plot_df_supp %>%
+        fmutate(`Suppression Flag` = ifelse(!is.na(wasRedacted) & wasRedacted, "Yes", "No")) %>%
+        fselect(-n, -wasRedacted, -n_orig),
+      how = "left"
+    ) %>%
+    fmutate(
+      frac_export = scales::percent(frac_export, accuracy = 0.1)
+    ) %>%
+    fselect(c(
+      unlist(selections),
+      "n_orig",
+      "num",
+      "frac_export",
+      "Suppression Flag")
+    ) %>%
+    frename(
+      'Total Count' = n_orig, 
+      'Permanent Count' = num, 
+      'Percent in Permanent' = frac_export
+    ) %>%
+    roworderv(tail(selections, 1))
+  
+  section_selections <- if (length(selections) == 2) rev(selections) else selections
+  
+  names(export_df)[seq_along(section_selections)] <- paste0(
+    section_selections,
+    " (Demographic Section ", seq_along(section_selections), ")"
+  )
+  
+  return(export_df)
+}
+
+syse_phd_plot <- function(methodology_type, selections, isExport = FALSE) {
   # 1. Prep Total Exits Universe
   raw_exits <- all_filtered_syse_demog()
-  total_prep <- prepare_crosstab_data(raw_exits, selections, methodology_type, subtab = subtab)
+  total_prep <- prepare_crosstab_data(raw_exits, selections, methodology_type, subtab = "exits")
   
   # 2. Prep Permanent Housing (PH) Exits Universe
   ph_exits <- raw_exits %>% fsubset(Destination %in% perm_livingsituation)
-  ph_prep    <- prepare_crosstab_data(ph_exits, selections, methodology_type, subtab = subtab)
+  ph_prep  <- prepare_crosstab_data(ph_exits, selections, methodology_type, subtab = "exits")
   
   # 3. Suppress Total Counts & Join with PH Counts
   plot_df_supp <- total_prep$plot_df %>% 
     suppress_values("n", keep_orig_var = TRUE)
+  
   for (s in selections) {
     plot_df_supp <- plot_df_supp %>% suppress_next_val_if_one_suppressed_in_group(s, "n")
   }
   
-  plot_df_joined <- join(plot_df_supp, ph_prep$plot_df %>% frename(num = n), how = 'left', on = selections) %>% 
+  plot_df_joined <- join(
+    plot_df_supp, 
+    ph_prep$plot_df %>% frename(num = n), 
+    how = 'left', 
+    on = selections
+  ) %>% 
     fmutate(
       frac = ifelse(n == 0 | is.na(n), NA, num / n),
       frac_export = ifelse(n_orig == 0 | is.na(n_orig), 0, num / n_orig)
@@ -53,6 +95,37 @@ sys_phd_plot <- function(subtab = 'phd', methodology_type, selections, isExport 
   syse_phd_plot_df(total_prep$plot_df)
   syse_phd_export(create_phd_export_df(plot_df_joined, plot_df_supp, selections))
   
+  # 5. Calc margin totals (needs Phd dataset, vs. Syso Comp, which doesn't)
+  margin_totals <- NULL
+  if (length(selections) == 2 && methodology_type == 1) {
+    margin_totals <- calc_margin_totals(total_prep$plot_df, selections[1], selections[2]) # uses helper from earlier
+    
+    h_total_phd <- ph_prep$plot_df %>%
+      fgroup_by(selections[2]) %>%
+      fsummarise(num = if (all(is.na(n))) NA_real_ else fsum(n)) %>%
+      mutate(!!sym(selections[1]) := 'Total')
+    
+    h_total_joined <- join(margin_totals$h_total, h_total_phd, how = 'left', on = selections) %>%
+      fmutate(
+        frac = ifelse(N == 0 | is.na(N), NA_real_, num / N),
+        val_col = frac
+      )
+    
+    v_total_phd <- ph_prep$plot_df %>%
+      fgroup_by(selections[1]) %>%
+      fsummarise(num = if (all(is.na(n))) NA_real_ else fsum(n)) %>%
+      mutate(!!sym(selections[2]) := 'Total')
+    
+    v_total_joined <- join(margin_totals$v_total, v_total_phd, how = 'left', on = selections) %>%
+      fmutate(
+        frac = ifelse(N == 0 | is.na(N), NA_real_, num / N),
+        val_col = frac
+      )
+    
+    margin_totals$h_total <- h_total_joined
+    margin_totals$v_total <- v_total_joined
+  }
+  
   # 5. Render Heatmap
   build_demographic_heatmap(
     plot_df = plot_df_joined,
@@ -60,7 +133,7 @@ sys_phd_plot <- function(subtab = 'phd', methodology_type, selections, isExport 
     metric_type = "ratio",
     palette_type = "green",
     methodology_type = methodology_type,
-    margin_totals = if (length(selections) == 2 && methodology_type == 1) list(h_total = h_total_joined, v_total = v_total_joined) else NULL,
+    margin_totals = margin_totals,
     total_count_display = syse_total_count_display(nrow(total_prep$clean_df), nrow(ph_prep$clean_df)),
     isExport = isExport
   )
