98d6519ac83fddd86fb093a6f6df4650ed4d81d8
Sara Sokolinski <sokolinskis@aneprrstu-01.ad.abt.local>
2026-08-26 14:15:00 -0400

    update change log

--- rcode/server_00_changelog.R
+++ rcode/server_00_changelog.R
@@ -3,6 +3,9 @@ output$changelog <- renderDT({
   changelog_dt <- tribble(
     ~ Date,
     ~ Change,
+    "08-26-2026", 
+    "<b>Bug Fixes</b> <br>
+      - Updated 'Enrollment Crosses Operating End' error to only flag for enrollments associated with projects that have an Operating End Date that is within the export dates.",
     "08-06-2026",
     "<b>Bug Fixes</b> <br>
       - Remove enrollments with no Bed Nights for 'Bed night entered on Project Exit Date' check (Issue <a href='https://github.com/abtassociates/eva/issues/1031' target='_blank'>#1031</a>)
