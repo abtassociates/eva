566eb00b14ecc4c59bf203360056bd2ea871411c
Alex Silverman <silvermana@aneprrstu-01.ad.abt.local>
2026-08-28 15:01:07 -0400

    no longer splitting Null Unless/Not Null When counts into separate issues.

--- util/machine_readable_specs_helpers.R
+++ util/machine_readable_specs_helpers.R
@@ -200,11 +200,6 @@ run_templatable_validations <- function(target_source, data_env = parent.frame()
               eval(parse(text = cond), envir = invalid_dt),
               stringi::stri_replace_all_fixed(detail_template, "Since {Name} has been collected in HMIS, please ensure that {readable_validation_notes} in HMIS.", "{Name} is missing in HMIS even though {readable_validation_notes}."),
               detail_template
-            ),
-            Issue = fifelse(
-              eval(parse(text = cond), envir = invalid_dt),
-              "Not Null When",
-              Issue
             )
           )
       }
