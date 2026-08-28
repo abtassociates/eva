ddd3e76389bdeab4630d0740da988bdd2e200f9c
Alex Silverman <silvermana@aneprrstu-01.ad.abt.local>
2026-08-28 17:35:36 -0400

    removing memory trackign functions, as those will be handled by 1051-speed-up-mirai-serialization

--- util/helper_functions.R
+++ util/helper_functions.R
@@ -795,19 +795,3 @@ intentional_stop <- function(session, message) {
     )
   )
 }
-
-
-# Memory tracking
-get_rss <- function() {
-  status <- readLines("/proc/self/status")
-  as.numeric(
-    sub("VmRSS:\\s+([0-9]+) kB", "\\1", status[grep("^VmRSS:", status)])
-  ) / 1024^2
-}
-log_memory <- function(session, label) {
-  logToConsole(session, sprintf(
-    "[MEMORY] %-40s %.2f GB",
-    label,
-    get_rss()
-  ))
-}
