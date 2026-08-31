a4cabde7e03c86bdfdc377b51ff522471291d210
Sara Sokolinski <sokolinskis@aneprrstu-01.ad.abt.local>
2026-08-31 15:01:22 -0400

    combine change logs under the next push date (9/3/26)

--- rcode/server_00_changelog.R
+++ rcode/server_00_changelog.R
@@ -3,13 +3,11 @@ output$changelog <- renderDT({
   changelog_dt <- tribble(
     ~ Date,
     ~ Change,
-    "08-26-2026",
-    "<b>Bug Fixes</b> <br>
-      - Added new DQ check to flag HoHs that are exited earlier than other household members in an enrollment.",
-    "08-06-2026",
+    "09-03-2026",
     "<b>Bug Fixes</b> <br>
       - Remove enrollments with no Bed Nights for 'Bed night entered on Project Exit Date' check (Issue <a href='https://github.com/abtassociates/eva/issues/1031' target='_blank'>#1031</a>)
-      - Move HouseholdType filter on <strong>Exits by Subpopulation</strong> page to the chart selector section (Issue <a href='https://github.com/abtassociates/eva/issues/1023' target='_blank'>#1023</a>.)",
+      - Move HouseholdType filter on <strong>Exits by Subpopulation</strong> page to the chart selector section (Issue <a href='https://github.com/abtassociates/eva/issues/1023' target='_blank'>#1023</a>.)
+      - Added new DQ check to flag HoHs that are exited earlier than other household members in an enrollment.",
     "08-03-2026",
     "<b>Bug Fixes</b> <br>
       - Fixed System Exits Client-level Download functionality. It was causing an error when writing the output to an Excel file.<br>
