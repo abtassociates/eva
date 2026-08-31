0b8bd6708aec5a6de1a754530b4c01599e090cf1
Sara Sokolinski <sokolinskis@aneprrstu-01.ad.abt.local>
2026-08-31 15:19:30 -0400

    # server_00_changelog.R
    - combine changes under next push date (9/3/26)
    - add link to new change log message for this ticket (1060)
    - remove misc period in second bullet (1023)

--- rcode/server_00_changelog.R
+++ rcode/server_00_changelog.R
@@ -3,13 +3,11 @@ output$changelog <- renderDT({
   changelog_dt <- tribble(
     ~ Date,
     ~ Change,
-    "08-26-2026", 
+    "09-03-2026", 
     "<b>Bug Fixes</b> <br>
-      - Updated 'Enrollment Crosses Operating End' error to only flag for enrollments associated with projects that have an Operating End Date that is within the export dates.",
-    "08-06-2026",
-    "<b>Bug Fixes</b> <br>
-      - Remove enrollments with no Bed Nights for 'Bed night entered on Project Exit Date' check (Issue <a href='https://github.com/abtassociates/eva/issues/1031' target='_blank'>#1031</a>)
-      - Move HouseholdType filter on <strong>Exits by Subpopulation</strong> page to the chart selector section (Issue <a href='https://github.com/abtassociates/eva/issues/1023' target='_blank'>#1023</a>.)",
+      - Remove enrollments with no Bed Nights for 'Bed night entered on Project Exit Date' check (Issue <a href='https://github.com/abtassociates/eva/issues/1031' target='_blank'>#1031</a>) <br>
+      - Move HouseholdType filter on <strong>Exits by Subpopulation</strong> page to the chart selector section (Issue <a href='https://github.com/abtassociates/eva/issues/1023' target='_blank'>#1023</a>) <br>
+      - Updated 'Enrollment Crosses Operating End' error to only flag for enrollments associated with projects that have an Operating End Date that is within the export dates. (Issue <a href='https://github.com/abtassociates/eva/issues/1060' target='_blank'>#1060</a>)",
     "08-03-2026",
     "<b>Bug Fixes</b> <br>
       - Fixed System Exits Client-level Download functionality. It was causing an error when writing the output to an Excel file.<br>
