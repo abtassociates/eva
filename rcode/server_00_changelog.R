36302b676e8a2297a8f6439c9da13bf0ffbc9ed3
Sara Sokolinski <sokolinskis@aneprrstu-01.ad.abt.local>
2026-09-03 14:49:57 -0400

    # server_00_changelog.R - move this check into placeholder "NEXT-2026" change date

--- rcode/server_00_changelog.R
+++ rcode/server_00_changelog.R
@@ -3,11 +3,13 @@ output$changelog <- renderDT({
   changelog_dt <- tribble(
     ~ Date,
     ~ Change,
+    "NEXT-2026",
+    "<b>Bug Fixes</b> <br>
+      - Updated 'Enrollment Crosses Operating End' error to only flag for enrollments associated with projects that have an Operating End Date that is within the export dates. (Issue <a href='https://github.com/abtassociates/eva/issues/1060' target='_blank'>#1060</a>)",
     "09-03-2026", 
     "<b>Bug Fixes</b> <br>
       - Remove enrollments with no Bed Nights for 'Bed night entered on Project Exit Date' check (Issue <a href='https://github.com/abtassociates/eva/issues/1031' target='_blank'>#1031</a>) <br>
-      - Move HouseholdType filter on <strong>Exits by Subpopulation</strong> page to the chart selector section (Issue <a href='https://github.com/abtassociates/eva/issues/1023' target='_blank'>#1023</a>) <br>
-      - Updated 'Enrollment Crosses Operating End' error to only flag for enrollments associated with projects that have an Operating End Date that is within the export dates. (Issue <a href='https://github.com/abtassociates/eva/issues/1060' target='_blank'>#1060</a>)",
+      - Move HouseholdType filter on <strong>Exits by Subpopulation</strong> page to the chart selector section (Issue <a href='https://github.com/abtassociates/eva/issues/1023' target='_blank'>#1023</a>)",
     "08-03-2026",
     "<b>Bug Fixes</b> <br>
       - Fixed System Exits Client-level Download functionality. It was causing an error when writing the output to an Excel file.<br>
