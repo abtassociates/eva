10c5372e84a123abad46b4d57908de220d2551cd
Marschall Furman <51181259+mlfurman3@users.noreply.github.com>
2026-09-16 15:55:13 -0400

    correct two more typos/edits that are specific to project performance metrics, not cosmetic ui changes

--- rcode/04_initial_data_prep.R
+++ rcode/04_initial_data_prep.R
@@ -124,7 +124,8 @@ EnrollmentStaging <- Enrollment %>%
             "UN"
           )
         )
-      )
+      ), 
+    levels = c("AOminusUY", "ACminusPY", "CO", "UN", "PY", "UY")
     )
   ) %>%
   fungroup()
@@ -424,7 +425,6 @@ session$userData$Enrollment <- Enrollment
 session$userData$CurrentLivingSituation <- CurrentLivingSituation
 session$userData$Assessment <- Assessment
 session$userData$Event <- Event
-
 # desk_time_providers <- validation() %>%
 #   dplyr::filter(
 #     (entered_between(., today() - years(1), today()) |
